5bef67c3bb70f94447385ba206fb16863ed35f99
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-03-14 14:07:22 +1100

    bring in Murrindindi configuration

---
+++ Murrindindi/SQL/COUNCIL_PropertyParcel.sql
@@ -0,0 +1,13 @@
+
+
+Select distinct TMPCounParc.*, TMPCounProp.address_pr AS address_pr, cparcount.MultiAssCount AS MultiAssCount, cpropcount.MultiParcCount AS MultiParcCount
+FROM Temp_PIQA_Parcel  as TMPCounParc 
+ INNER JOIN 
+	Council_parcel_Count AS cparcount ON TMPCounParc.SPI =  cparcount.SPI
+	
+INNER JOIN 
+	Temp_PIQA_Address AS TMPCounProp ON TMPCounParc.crefno =  TMPCounProp.crefno
+
+	INNER JOIN 
+	Council_Property_Count AS cpropcount ON TMPCounParc.propnum =  cpropcount.Propnum
+
