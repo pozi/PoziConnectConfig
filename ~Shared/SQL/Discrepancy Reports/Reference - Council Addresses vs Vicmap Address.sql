6ec1032a56136917f98718a529cbe7e7f1adb98f
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-09-16 12:12:49 +1000

    Exclude NCPR records from address discrepancies

--- ~Shared/SQL/Discrepancy Reports/Reference - Council Addresses vs Vicmap Address.sql
+++ ~Shared/SQL/Discrepancy Reports/Reference - Council Addresses vs Vicmap Address.sql
@@ -8,6 +8,7 @@ from
     PC_Vicmap_Property_Address vicmap_address
 where
     council_address.propnum = vicmap_address.propnum and
+    council_address.propnum <> 'NCPR' and
     council_address.num_road_address <> vicmap_address.num_road_address and    
     vicmap_address.is_primary <> 'N' and    
     council_address.is_primary <> 'N'
