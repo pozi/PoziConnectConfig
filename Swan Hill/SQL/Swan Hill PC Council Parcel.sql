dac4c393d301647ef7559ae7be40cf9b0ddc9f4c
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-09-17 00:11:31 +1000

    Major update to Swan Hill parcel logic

--- Swan Hill/SQL/Swan Hill PC Council Parcel.sql
+++ Swan Hill/SQL/Swan Hill PC Council Parcel.sql
@@ -2,6 +2,7 @@ select
     *,
     case
         when plan_number <> '' and lot_number = '' then plan_number
+        when plan_number <> '' and sec <> '' then lot_number || '~' || sec || '\' || plan_number
         when plan_number <> '' then lot_number || '\' || plan_number
         when ( parish_code <> '' or township_code <> '' ) then
             allotment ||
@@ -12,17 +13,18 @@ select
     end as spi,
     case
         when plan_numeral <> '' and lot_number = '' then plan_numeral
+        when plan_number <> '' and sec <> '' then lot_number || '~' || sec || '\' || plan_numeral
         when plan_numeral <> '' then lot_number || '\' || plan_numeral
         when ( parish_code <> '' or township_code <> '' ) then
             allotment ||
             case when sec <> '' then '~' || sec else '' end ||
-            '\PP' ||
+            '\' ||
             case when township_code <> '' then township_code else parish_code end
         else ''
     end as simple_spi
 from
 (
-select
+select distinct
     cast ( auprparc.ass_num as varchar ) as propnum,
     case auprparc.pcl_flg
         when 'R' then 'A'
@@ -31,74 +33,75 @@ select
     end as status,
     cast ( auprparc.pcl_num as varchar ) as crefno,
     case
-        when auprparc.ttl_cde = 6 OR auprparc.ttl_no2 in ( 'P' , 'PT' ) then 'P'
+        when auprparc.ttl_cde in ( 10 , 19 , 20 ) then 'P'
         else ''
     end as part,
     case
-        when auprparc.ttl_cde = 1 then 'LP'
-        when auprparc.ttl_cde = 2 then 'PC'
-        when auprparc.ttl_cde = 5 then 'SP'
-        when auprparc.ttl_cde = 7 then 'RP'
-        when auprparc.ttl_cde = 12 then 'TP'
-        when auprparc.ttl_cde = 13 then 'PS'
+        when auprparc.ttl_cde not in ( 9 , 10 , 27 ) then
+            case
+                when auprparc.ttl_cde in ( 1 , 2 , 13 , 18 , 19 , 20 , 21 ) then 'PS'
+                when auprparc.ttl_cde in ( 3 , 4 ) then 'LP'
+                when auprparc.ttl_cde in ( 5 , 12 ) then 'TP'
+                when auprparc.ttl_cde = 6 then 'PC'
+                when auprparc.ttl_cde = 7 then 'CP'
+                when auprparc.ttl_cde = 8 then 'RP'
+                when auprparc.ttl_cde = 11 then 'SP'
+                when auprparc.ttl_cde = 22 then 'CS'
+                else ''
+            end ||
+                case
+                    when null then ''
+                    when substr ( cast ( auprparc.ttl_in5 as varchar ) , -1 , 1 ) in ( '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' ) then cast ( auprparc.ttl_in5 as varchar )
+                    when substr ( cast ( auprparc.ttl_in5 as varchar ) , -1 , 1 ) not in ( '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' ) then substr ( cast ( auprparc.ttl_in5 as varchar ) , 1 , length ( cast ( auprparc.ttl_in5 as varchar ) ) - 1 )        
+                    else ''
+                end
         else ''
-    end ||
-        case
-            when null then ''
-            when substr ( trim ( auprparc.ttl_no5 ) , -1 , 1 ) in ( '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' ) then trim ( auprparc.ttl_no5 )
-            when substr ( trim ( auprparc.ttl_no5 ) , -1 , 1 ) not in ( '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' ) then substr ( trim ( auprparc.ttl_no5 ) , 1 , length ( trim ( auprparc.ttl_no5 ) ) - 1 )        
-            else ''
-        end as plan_number,
+    end as plan_number,
     case
-        when auprparc.ttl_cde = 1 then 'LP'
-        when auprparc.ttl_cde = 2 then 'PC'
-        when auprparc.ttl_cde = 5 then 'SP'
-        when auprparc.ttl_cde = 7 then 'RP'
-        when auprparc.ttl_cde = 12 then 'TP'
-        when auprparc.ttl_cde = 13 then 'PS'
+        when auprparc.ttl_cde in ( 1 , 2 , 13 , 18 , 19 , 20 , 21 ) then 'PS'
+        when auprparc.ttl_cde in ( 3 , 4 ) then 'LP'
+        when auprparc.ttl_cde in ( 5 , 12 ) then 'TP'
+        when auprparc.ttl_cde = 6 then 'PC'
+        when auprparc.ttl_cde = 7 then 'CP'
+        when auprparc.ttl_cde = 8 then 'RP'
+        when auprparc.ttl_cde = 11 then 'SP'
+        when auprparc.ttl_cde = 22 then 'CS'
         else ''
     end as plan_prefix,
     case
-        when auprparc.ttl_cde in ( 1 , 2 , 5 , 7 , 12 , 13 ) then ifnull ( auprparc.ttl_no5 , '' )
-        else ''
-    end as plan_numeral,
-    case
-        when auprparc.ttl_cde in ( 1 , 2 , 5 , 7 , 12 , 13 ) then ifnull ( auprparc.ttl_no1 , '' )
-        else ''
-    end as lot_number,
-    case
-        when auprparc.ttl_cde in ( 3 , 6 ) then trim ( auprparc.ttl_no5 ) || ifnull ( auprparc.ttl_no6 , '' )
-        else ''
-    end as allotment,
-    case
-        when auprparc.ttl_cde = 3 then ifnull ( auprparc.ttl_no4 , '' )
-        when auprparc.ttl_cde = 6 then ifnull ( auprparc.ttl_no3 , '' )
-        else ''
-    end as sec,
-    case
-        when auprparc.ttl_cde in ( 3 , 6 ) then
-            case upper ( aualrefn.dsc_no1 )
-                when 'TYNTYNDER WEST' then '3676'
+        when auprparc.ttl_cde not in ( 9 , 10 , 27 ) then
+            case    
+                when null then ''
+                when substr ( cast ( auprparc.ttl_in5 as varchar ) , -1 , 1 ) in ( '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' ) then cast ( auprparc.ttl_in5 as varchar )
+                when substr ( cast ( auprparc.ttl_in5 as varchar ) , -1 , 1 ) not in ( '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' ) then substr ( cast ( auprparc.ttl_in5 as varchar ) , 1 , length ( cast ( auprparc.ttl_in5 as varchar ) ) - 1 )        
                 else ''
             end
         else ''
-    end as parish_code,
+    end as plan_numeral,
+    case
+        when auprparc.ttl_cde not in ( 9 , 10 , 27 ) then ifnull ( auprparc.ttl_no1 , '' )
+        else ''
+    end as lot_number,
+    case
+        when auprparc.ttl_cde in ( 9 , 10 , 27 ) then ifnull ( auprparc.ttl_no1 , '' )
+        else ''
+    end as allotment,
+    case
+        when auprparc.ttl_cde in ( 9 , 10 , 27 ) then ifnull ( auprparc.ttl_no5 , '' )
+        else ifnull ( auprparc.ttl_no3 , '' )
+    end as sec,
+    aualrefn.ref_val as parish_code,
     '' as township_code,
     fmt_ttl as summary,
     '366' as lga_code
 from
     AUTHORITY_auprparc auprparc,
---    AUTHORITY_auprstad auprstad,
---    AUTHORITY_ausrsubr ausrsubr,
     AUTHORITY_aualrefn aualrefn
 where
---    auprparc.pcl_num = auprstad.pcl_num and
---    auprstad.sbr_nme = ausrsubr.sbr_nme and
     auprparc.pcl_flg in ( 'R' , 'P' ) and
     auprparc.ass_num is not null and
     auprparc.udn_cd1 = aualrefn.ref_val and
-    aualrefn.ref_typ = 'udn_cd1' and
-    ttl_no1 not like 'CM%'
+    aualrefn.ref_typ = 'udn_cd1'
 order by
     ass_num
 )
