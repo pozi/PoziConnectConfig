c065c3ee5022ccfe0abe83b05207c65da265ce28
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-09-19 23:37:26 +1000

    Upgrade Wangaratta config to new version

---
+++ Wangaratta/SQL/Wangaratta PC Council Parcel.sql
@@ -0,0 +1,71 @@
+select
+    *,
+    case
+        when plan_number <> '' and lot_number = '' then plan_number
+        when plan_number <> '' then lot_number || '\' || plan_number
+        when ( parish_code <> '' or township_code <> '' ) then
+            allotment ||
+            case when sec <> '' then '~' || sec else '' end ||
+            '\PP' ||
+            case when township_code <> '' then township_code else parish_code end
+        else ''
+    end as spi,
+    case
+        when plan_numeral <> '' and lot_number = '' then plan_numeral
+        when plan_numeral <> '' then lot_number || '\' || plan_numeral
+        when ( parish_code <> '' or township_code <> '' ) then
+            allotment ||
+            case when sec <> '' then '~' || sec else '' end ||
+            '\PP' ||
+            case when township_code <> '' then township_code else parish_code end
+        else ''
+    end as simple_spi
+from
+(
+select
+    case
+        when A.key1 in ( 14359 , 15127 , 14360 , 15893 , 15303 ) then 'NCPR'
+        else cast ( A.key1 as varchar )
+    end as propnum,
+    cast ( L.land_no as varchar ) as crefno,
+    case L.status
+        when 'F' then 'P'
+        else ''
+    end as status,		
+    case P.text2
+        when 'Yes' then 'Y'
+        else ''
+    end as distance_related_flag,
+    ifnull ( upper ( part_lot ) , '' ) as part,
+    case
+        when substr ( trim ( L.plan_no ) , -1 ) in ( '1','2','3','4','5','6','7','8','9','0' ) THEN trim ( L.plan_desc ) || L.plan_no
+        else ifnull ( trim ( L.plan_desc ) || substr ( trim ( L.plan_no ) , 1 , length ( trim ( L.plan_no ) ) - 1 ) , '' )
+    end as plan_number,
+    ifnull ( L.plan_desc , '' ) as plan_prefix,
+    case
+        when substr ( trim ( L.plan_no ) , -1 ) in ( '1','2','3','4','5','6','7','8','9','0' ) then L.plan_no
+        else ifnull ( substr ( trim ( L.plan_no ) , 1 , length ( trim ( L.plan_no ) ) - 1 ) , '' )
+    end as plan_numeral,
+    ifnull ( L.lot , '' ) as lot_number,
+    ifnull ( L.text3,'') as allotment,
+    ifnull ( L.parish_section , '' ) as sec,
+    '' as parish_code,
+    '' as township_code,
+    '368' as lga_code
+from
+    PROCLAIM_nucLand L
+    join PROCLAIM_nucAssociation A on L.land_no = A.key2 and
+    L.status in ( 'C' , 'F')
+    join PROCLAIM_nucProperty P on A.key1 = p.Property_no
+    left join PROCLAIM_nucAssociation T on A.key1 = T.key1 and
+    A.key2 = T.key2 and
+    T.association_type = 'TransPRLD' and
+    A.date_ended is null
+where
+    A.association_type = 'PropLand' and
+    A.date_ended is null and
+    L.plan_desc in ( 'TP' , 'LP' , 'PS' , 'PC' , 'CP' , 'SP' , 'CS' ,'RP','CG' ) and
+    T.key1 is null and
+    p.property_no not in ('16953','16899','17736','15127','14359','15893','14360','15303') and
+    P.status in ( 'C' , 'c' )
+)
