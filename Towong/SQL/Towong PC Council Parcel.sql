4c51d4be7c79191f4a7d38083318ee67eef563f6
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-09-23 09:06:54 +1000

    Progress on updating parcel config to new version

--- Towong/SQL/Towong PC Council Parcel.sql
+++ Towong/SQL/Towong PC Council Parcel.sql
@@ -1,38 +1,33 @@
-SELECT [Lynx_vwLandParcel].[PropertyNumber] AS [Propnum],
-  [Lynx_vwLandParcel].[LandParcelNumber] AS [Crefno],
-  [Lynx_vwLandParcel].[Type],
-  [Lynx_vwLandParcel].[Lot],
-  [Lynx_vwLandParcel].[PlanNo],
-  [Lynx_vwLandParcel].[CrownAllotment],
-  [Lynx_vwLandParcel].[Section],
-  [Lynx_vwLandParcel].[Township],
-  [Lynx_vwLandParcel].[Parish],
-  CASE
-    WHEN [Lynx_vwLandParcel].[Type] = 'Lodged Plan' THEN
-    [Lynx_vwLandParcel].[Lot] || '\LP' || [Lynx_vwLandParcel].[PlanNo]
-    WHEN [Lynx_vwLandParcel].[Type] = 'Title Plan' THEN
-    [Lynx_vwLandParcel].[Lot] || '\TP' || [Lynx_vwLandParcel].[PlanNo]
-    WHEN [Lynx_vwLandParcel].[Type] = 'Plan of Subdivision' THEN
-    Replace([Lynx_vwLandParcel].[Lot], 'Proposed-', '') || '\PS' ||
-    [Lynx_vwLandParcel].[PlanNo]
-    WHEN [Lynx_vwLandParcel].[Type] = 'Consolidation Plan' THEN 'PC' ||
-    [Lynx_vwLandParcel].[PlanNo]
-    WHEN [Lynx_vwLandParcel].[Type] = 'Stratum Plan' THEN
-    [Lynx_vwLandParcel].[Lot] || '\SP' || [Lynx_vwLandParcel].[PlanNo]
-    WHEN [Lynx_vwLandParcel].[Type] = 'Crown Description' AND
-    [Lynx_vwLandParcel].[Township] IS NULL THEN
-    Replace([Lynx_vwLandParcel].[CrownAllotment] || '~' ||
-    [Lynx_vwLandParcel].[Section] || '\PP' || [DSE_VMADMIN_PARISH].[PARISHC],
-    '~NO', '')
-    WHEN [Lynx_vwLandParcel].[Type] = 'Crown Description' AND
-    [Lynx_vwLandParcel].[Township] IS NOT NULL THEN
-    Replace([Lynx_vwLandParcel].[CrownAllotment] || '~' ||
-    [Lynx_vwLandParcel].[Section] || '\PP' || [DSE_VMADMIN_TOWNSHIP].[TOWNSHIPC], '~NO', '') ELSE '' END AS [SPI],
-  [DSE_VMADMIN_PARISH].[PARISHC],
-  [DSE_VMADMIN_TOWNSHIP].[TOWNSHIPC]
-FROM [Lynx_vwLandParcel]
-  LEFT JOIN [DSE_VMADMIN_PARISH] ON UPPER([Lynx_vwLandParcel].[Parish]) =
-    [DSE_VMADMIN_PARISH].[PARISH]
-  LEFT JOIN [DSE_VMADMIN_TOWNSHIP] ON UPPER([Lynx_vwLandParcel].[Township]) =
-    REPLACE([DSE_VMADMIN_TOWNSHIP].[TOWNSHIP], ' TP','')
-WHERE [Lynx_vwLandParcel].[Status] = 'Active' AND [Lynx_vwLandParcel].[Ended] IS NULL
+select
+  Parcel.PropertyNumber as propnum,
+  Parcel.LandParcelNumber as crefno,  
+  '' as status,
+  '' as part,
+  case Parcel.Type  
+    when 'Lodged Plan' then 'LP' || Parcel.PlanNo
+    when 'Title Plan' then 'TP' || Parcel.PlanNo
+    when 'Plan of Subdivision' then 'PS' || Parcel.PlanNo
+    when 'Consolidation Plan' then 'PC' || Parcel.PlanNo
+    when 'Stratum Plan' then 'SP' || Parcel.PlanNo
+    else ''
+  end as plan_number,
+  case Parcel.Type  
+    when 'Lodged Plan' then 'LP'
+    when 'Title Plan' then 'TP'
+    when 'Plan of Subdivision' then 'PS'
+    when 'Consolidation Plan' then 'PC'
+    when 'Stratum Plan' then 'SP'
+    else ''
+  end as plan_prefix,
+  Parcel.PlanNo as plan_numeral,
+  Parcel.Lot as lot_number,
+  Parcel.CrownAllotment as allotment,
+  Parcel.Section as sec,
+  Parcel.Parish asparish_code,
+  Parcel.Township as township_code,
+  '367' as lga_code
+from
+  Lynx_vwLandParcel Parcel
+where
+  Parcel.Status = 'Active' and
+  Parcel.Ended is null
