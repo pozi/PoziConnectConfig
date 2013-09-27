0cfc233eb3c5d9c93d38001857c556c1f10345dc
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-09-27 10:44:38 +1000

    New Cardinia configuration

--- Cardinia/README.md
+++ Cardinia/README.md
@@ -1,1 +1,11 @@
 # Cardinia
+
+### SQL Files
+
+* parcel SQL based on `Mount Alexander PC Parcel.sql`
+* property address SQL based on `Mount Alexander PC Property Address.sql`
+
+### INI Files
+
+* all based on Mount Alexander
+
---
+++ Cardinia/SQL/Cardinia PC Council Parcel.sql
@@ -0,0 +1,56 @@
+select
+    *,
+    case
+        when plan_number <> '' and lot_number = '' then plan_number
+        when plan_number <> '' then lot_number || '\' || plan_number
+        when ( parish_code <> '' or township_code <> '' ) then
+            allotment ||
+            case when sec <> '' then '~' || sec else '' end ||
+            '\PP' ||
+            case when township_code <> '' then township_code else parish_code end
+        else ''
+    end as spi,
+    case
+        when plan_numeral <> '' and lot_number = '' then plan_numeral
+        when plan_numeral <> '' then lot_number || '\' || plan_numeral
+        when ( parish_code <> '' or township_code <> '' ) then
+            allotment ||
+            case when sec <> '' then '~' || sec else '' end ||
+            '\PP' ||
+            case when township_code <> '' then township_code else parish_code end
+        else ''
+    end as simple_spi
+from (
+
+select 
+    cast ( cast ( Assessment.Assess_Number as integer ) as varchar ) as propnum,
+    '' as status,
+    cast ( Title.Title_Id as varchar ) as crefno,
+    Title.Title_Legal_Description as summary,
+    case when Title_Is_Part_of_Lot = 1 then 'P' else '' end as part,
+    ifnull ( Plan_Type.Plan_Type_Code || Title.Title_Plan_Number , '' ) as plan_number,
+    ifnull ( Plan_Type.Plan_Type_Code , '' ) as plan_prefix,
+    ifnull ( Title.Title_Plan_Number ,'' ) as plan_numeral,
+    ifnull ( Title_Lot , '' ) as lot_number,
+    ifnull ( Title.Title_Crown_Allotment , '' ) as allotment,
+    ifnull ( Title.Title_Section , '' ) as sec,
+    ifnull ( Title.Title_Block , '' ) as block,
+    ifnull ( Title.Title_Portion , '' ) as portion,
+    ifnull ( Title.Title_Subdivision , '' ) as subdivision,
+    ifnull ( Parish.Parish_Code , '' ) as parish_code,
+    '' as township_code,
+    '311' as lga_code
+from
+    PropertyGov_parcel as Parcel inner join
+    PropertyGov_parcel_title as Parcel_Title on Parcel.Parcel_Id = Parcel_Title.Parcel_Id inner join
+    PropertyGov_title as Title on Parcel_Title.Title_Id = Title.Title_Id inner join
+    PropertyGov_assessment_parcel as Assessment_Parcel on Parcel.Parcel_Id = Assessment_Parcel.Parcel_Id inner join
+    PropertyGov_assessment as Assessment on Assessment_Parcel.Assessment_Id = Assessment.Assessment_Id left outer join
+    PropertyGov_plan_type as Plan_Type on Title.Plan_Type = Plan_Type.Plan_Type left outer join
+    PropertyGov_parish as Parish on Title.Parish_Id = Parish.Parish_Id
+where
+    Parcel.Parcel_Status = 0 and
+    Assessment.Assessment_Status not in ( '9' , '22' ) and    
+    Assessment.Assess_Number is not null
+order by propnum, crefno
+)
