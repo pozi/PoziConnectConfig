9702eebcec75458aae048b508a4bfe890c46f555
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-12-10 14:28:19 +1100

    Replace tabs with spaces

--- Towong/SQL/Towong PC Council Property Address.sql
+++ Towong/SQL/Towong PC Council Property Address.sql
@@ -27,7 +27,7 @@ select
 from (
 
 select
-	cast ( Property.Property as varchar ) as propnum,
+    cast ( Property.Property as varchar ) as propnum,
     '' as status,
     '' as base_propnum,
     '' as is_primary,
@@ -35,75 +35,75 @@ select
     '' as hsa_flag,
     '' as hsa_unit_id,
     '' as location_descriptor,
-	'' as blg_unit_type,
-	'' as blg_unit_prefix_1,
-	ifnull ( Property.UnitNo , '' ) as blg_unit_id_1,
-	'' as blg_unit_suffix_1,
-	'' as blg_unit_prefix_2,
-	'' as blg_unit_id_2,
-	'' as blg_unit_suffix_2,
-	'' as floor_type,
-	'' as floor_prefix_1,
-	'' as floor_no_1,
-	'' as floor_suffix_1,
-	'' as floor_prefix_2,
-	'' as floor_no_2,
-	'' as floor_suffix_2,
-	ifnull ( Property.Name , '' ) as building_name,
-	'' as complex_name,
-	'' as house_prefix_1,
-	case
-		when substr ( Property.StreetNofrom , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-		when substr ( Property.StreetNofrom , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then Property.StreetNofrom
-		else ifnull ( substr ( Property.StreetNofrom , 1 , length ( Property.StreetNofrom ) - 1 ) , '' ) 
-	end as house_number_1,
-	case
-		when substr ( Property.StreetNofrom , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-		when substr ( Property.StreetNofrom , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-		else ifnull ( substr ( Property.StreetNofrom , -1 , 1 ) , '' )
-	end as house_suffix_1,
-	'' as house_prefix_2,
-	case
-		when substr ( Property.StreetNoTo , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-		when substr ( Property.StreetNoTo , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then Property.StreetNoTo 
-		else ifnull ( substr ( Property.StreetNoTo , 1 , length ( Property.StreetNoTo ) - 1 ) , '' ) 
-	end as house_number_2,
-	case
-		when substr ( Property.StreetNoTo , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-		when substr ( Property.StreetNoTo , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-		else ifnull ( substr ( Property.StreetNoTo , -1 , 1 ) , '' )
-	end as house_suffix_2,
-	replace ( upper ( Street.Name ) , ' - ' , '-' ) as road_name,
-	case
-		when StreetType.Type like 'Road %' then 'ROAD'
-		when StreetType.Type like 'Street %' then 'STREET'
-		else upper ( StreetType.Type )
-	end as road_type, 
-	case
-		when StreetType.Type like '% North' then 'N'
-		when StreetType.Type like '% South' then 'S'
-		when StreetType.Type like '% East' then 'E'
-		when StreetType.Type like '% West' then 'W'
-		else ''
-	end as road_suffix,
-	upper ( Locality.Locality ) as locality_name,
-	Locality.Postcode as postcode,
+    '' as blg_unit_type,
+    '' as blg_unit_prefix_1,
+    ifnull ( Property.UnitNo , '' ) as blg_unit_id_1,
+    '' as blg_unit_suffix_1,
+    '' as blg_unit_prefix_2,
+    '' as blg_unit_id_2,
+    '' as blg_unit_suffix_2,
+    '' as floor_type,
+    '' as floor_prefix_1,
+    '' as floor_no_1,
+    '' as floor_suffix_1,
+    '' as floor_prefix_2,
+    '' as floor_no_2,
+    '' as floor_suffix_2,
+    ifnull ( Property.Name , '' ) as building_name,
+    '' as complex_name,
+    '' as house_prefix_1,
+    case
+        when substr ( Property.StreetNofrom , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
+        when substr ( Property.StreetNofrom , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then Property.StreetNofrom
+        else ifnull ( substr ( Property.StreetNofrom , 1 , length ( Property.StreetNofrom ) - 1 ) , '' ) 
+    end as house_number_1,
+    case
+        when substr ( Property.StreetNofrom , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
+        when substr ( Property.StreetNofrom , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
+        else ifnull ( substr ( Property.StreetNofrom , -1 , 1 ) , '' )
+    end as house_suffix_1,
+    '' as house_prefix_2,
+    case
+        when substr ( Property.StreetNoTo , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
+        when substr ( Property.StreetNoTo , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then Property.StreetNoTo 
+        else ifnull ( substr ( Property.StreetNoTo , 1 , length ( Property.StreetNoTo ) - 1 ) , '' ) 
+    end as house_number_2,
+    case
+        when substr ( Property.StreetNoTo , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
+        when substr ( Property.StreetNoTo , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
+        else ifnull ( substr ( Property.StreetNoTo , -1 , 1 ) , '' )
+    end as house_suffix_2,
+    replace ( upper ( Street.Name ) , ' - ' , '-' ) as road_name,
+    case
+        when StreetType.Type like 'Road %' then 'ROAD'
+        when StreetType.Type like 'Street %' then 'STREET'
+        else upper ( StreetType.Type )
+    end as road_type, 
+    case
+        when StreetType.Type like '% North' then 'N'
+        when StreetType.Type like '% South' then 'S'
+        when StreetType.Type like '% East' then 'E'
+        when StreetType.Type like '% West' then 'W'
+        else ''
+    end as road_suffix,
+    upper ( Locality.Locality ) as locality_name,
+    Locality.Postcode as postcode,
     '' as access_type,
     '367' as lga_code,
     '' as crefno
 from
-	Lynx_vwPropertyClassification Classification,
-	Lynx_Propertys Property,
-	Lynx_Streets Street,
-	Lynx_StreetType StreetType,
-	Lynx_Localities Locality
+    Lynx_vwPropertyClassification Classification,
+    Lynx_Propertys Property,
+    Lynx_Streets Street,
+    Lynx_StreetType StreetType,
+    Lynx_Localities Locality
 where
-	Classification.PropertyNumber = Property.Property and
-	Property.StreetID = Street.ID and
-	Street.Type = StreetType.ID and
-	Street.Locality = Locality.ID and
-	Property.Type not in ( 672 , 700 ) and
-	Classification.LandClassificationCode <> 010
+    Classification.PropertyNumber = Property.Property and
+    Property.StreetID = Street.ID and
+    Street.Type = StreetType.ID and
+    Street.Locality = Locality.ID and
+    Property.Type not in ( 672 , 700 ) and
+    Classification.LandClassificationCode <> 010
 )
 )
 )
