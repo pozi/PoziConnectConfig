e8da233f567fd60725c526e543f15e15ef69ad6f
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-12-16 09:47:29 +1100

    Commence rural address import config

---
+++ Swan Hill/SQL/Swan Hill PC Council Rural Address.sql
@@ -0,0 +1,9 @@
+update PC_Council_Property_Address
+set
+    is_primary = ( select case when [primary] = 'T' then 'Y' else 'N' end from SHRCC_Rural_Address where SHRCC_Rural_Address.ra_complete = PC_Council_Property_Address.num_road_address ),
+    distance_related_flag = 'Y' ,
+    easting = ( select xcoord from SHRCC_Rural_Address where SHRCC_Rural_Address.ra_complete = PC_Council_Property_Address.num_road_address ),
+    northing = ( select ycoord from SHRCC_Rural_Address where SHRCC_Rural_Address.ra_complete = PC_Council_Property_Address.num_road_address ),    
+    datum_proj = 'MGA Zone 54 (GDA 94)'
+where
+    num_road_address in ( select ra_complete from SHRCC_Rural_Address where ra_complete <> '' and xcoord > 0 and ycoord > 0 )
