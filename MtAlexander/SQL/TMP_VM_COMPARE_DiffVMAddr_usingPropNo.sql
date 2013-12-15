7109b343ea68f03a10a3b2532c377642c9733f60
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2013-12-16 09:44:10 +1100

    Remove old Mount Alexander config files

--- MtAlexander/SQL/TMP_VM_COMPARE_DiffVMAddr_usingPropNo.sql
+++
@@ -1,8 +0,0 @@
-Select distinct 
-c.PROPNUM AS PROPNUM, 
-vm.LGA_code as LGA_code, 
-c.address_pr, 
-vm.EZI_ADD_NO_PC 
-from Temp_PIQA_address c 
-inner join TMP_VM_PROPERTY_ADDRESS vm on c.PROPNUM = vm.PROPNUM 
-where c.address_pr <> vm.ezi_add_no_pc
