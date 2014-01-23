10f4740028b1a9a8f4d46da657fdffe5662f4fc3
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-01-24 09:25:36 +1100

    Filter query to include only parcels with valid propnum

--- ~Shared/SQL/PC Council Parcel Property Count.sql
+++ ~Shared/SQL/PC Council Parcel Property Count.sql
@@ -3,5 +3,6 @@ select
     status,
     count(*) as num_props
 from PC_Council_Parcel
+where propnum in ( select propnum from PC_Council_Property_Address )
 group by spi, status
 order by num_props desc
