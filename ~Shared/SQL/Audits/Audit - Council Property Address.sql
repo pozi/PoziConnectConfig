94e7a5a767716c9832bbc5f7889c9d63f59dd220
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-02-25 23:19:36 +1100

    Refine audits

--- ~Shared/SQL/Audits/Audit - Council Property Address.sql
+++ ~Shared/SQL/Audits/Audit - Council Property Address.sql
@@ -7,16 +7,16 @@ select
     summary as council_summary,
     ifnull ( ( select vppc.num_parcels from pc_vicmap_property_parcel_count vppc where vppc.propnum = cpa.propnum ) , 0 ) as num_parcels_in_vicmap,
     ifnull ( ( select vpa.num_road_address from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum limit 1 ) , '' ) as vicmap_address,
-    ifnull ( ( select vpa.num_locality_name from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum limit 1 ) , '' ) as vicmap_locality,
+    ifnull ( ( select vpa.locality_name from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum limit 1 ) , '' ) as vicmap_locality,
     case
         when propnum not in ( select vpa.propnum from pc_vicmap_property_address vpa ) then ''
         when num_road_address = ( select vpa.num_road_address from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum limit 1 ) then 'Y'
-        else 'F'
+        else 'N'
     end as address_match_in_vicmap,
     case
         when propnum not in ( select vpa.propnum from pc_vicmap_property_address vpa ) then ''
         when locality_name = ( select vpa.locality_name from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum limit 1 ) then 'Y'
-        else 'F'
+        else 'N'
     end as locality_match_in_vicmap,
     ifnull ( ( select edit_code from m1 where m1.propnum = cpa.propnum limit 1 ) , '' ) as current_m1_edit_code,
     ifnull ( ( select comments from m1 where m1.propnum = cpa.propnum limit 1 ) , '' ) as current_m1_comments
