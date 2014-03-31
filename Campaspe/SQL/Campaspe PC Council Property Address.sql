0618deecc3ed3139a86d096dd03ce25faf80574d
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-04-01 10:47:16 +1100

    Add table prefix

--- Campaspe/SQL/Campaspe PC Council Property Address.sql
+++ Campaspe/SQL/Campaspe PC Council Property Address.sql
@@ -33,55 +33,55 @@ select
     '' as is_primary,
     '' as distance_related_flag,
     '' as hsa_flag,
-    '' as hsa_unit_id,    
+    '' as hsa_unit_id,
     case
-        when ASS_HOUSE_NO_PREFIX like 'FACTORY%' then 'FACT'
-        when ASS_HOUSE_NO_PREFIX like 'FLAT%' then 'FLAT'
-        when ASS_HOUSE_NO_PREFIX like 'HALL%' then 'HALL'
-        when ASS_HOUSE_NO_PREFIX like 'OFFICE%' then 'OFFC'
-        when ASS_HOUSE_NO_PREFIX like 'SHED%' then 'SHED'
-        when ASS_HOUSE_NO_PREFIX like 'SHOP%' then 'SHOP'
-        when ASS_HOUSE_NO_PREFIX like 'SILO%' then 'SILO'
-        when ASS_HOUSE_NO_PREFIX like 'SUITE%' then 'SE'
-        when ASS_HOUSE_NO_PREFIX like 'UNIT%' then 'UNIT'
+        when a.ASS_HOUSE_NO_PREFIX like 'FACTORY%' then 'FACT'
+        when a.ASS_HOUSE_NO_PREFIX like 'FLAT%' then 'FLAT'
+        when a.ASS_HOUSE_NO_PREFIX like 'HALL%' then 'HALL'
+        when a.ASS_HOUSE_NO_PREFIX like 'OFFICE%' then 'OFFC'
+        when a.ASS_HOUSE_NO_PREFIX like 'SHED%' then 'SHED'
+        when a.ASS_HOUSE_NO_PREFIX like 'SHOP%' then 'SHOP'
+        when a.ASS_HOUSE_NO_PREFIX like 'SILO%' then 'SILO'
+        when a.ASS_HOUSE_NO_PREFIX like 'SUITE%' then 'SE'
+        when a.ASS_HOUSE_NO_PREFIX like 'UNIT%' then 'UNIT'
         else ''
     end as blg_unit_type,
-    '' as blg_unit_prefix_1,    
+    '' as blg_unit_prefix_1,
     case
-        when ASS_HOUSE_NO_PREFIX in ( 'ABOVE' , 'OFF' , '(OFF)' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then ''
-        when ASS_HOUSE_NO_PREFIX like 'FACTORY%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 8))
-        when ASS_HOUSE_NO_PREFIX like 'FLAT%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 5))
-        when ASS_HOUSE_NO_PREFIX like 'HALL%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 5))
-        when ASS_HOUSE_NO_PREFIX like 'OFFICE%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 7))
-        when ASS_HOUSE_NO_PREFIX like 'SHED%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 5))
-        when ASS_HOUSE_NO_PREFIX like 'SHOP%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 5))
-        when ASS_HOUSE_NO_PREFIX like 'SILO%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 5))
-        when ASS_HOUSE_NO_PREFIX like 'SUITE%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 6))
-        when ASS_HOUSE_NO_PREFIX like 'UNIT%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 5))
-        when upper(substr(ASS_HOUSE_NO_PREFIX, -1)) between 'A' and 'Z' then substr(ASS_HOUSE_NO_PREFIX,1,length(ASS_HOUSE_NO_PREFIX)-1)
-        when ASS_HOUSE_NO_PREFIX like '%-%' then 'XXXX'
-        when ASS_HOUSE_NO_PREFIX like '%&%' then 'XXXX'
-        else ifnull ( ASS_HOUSE_NO_PREFIX , '' )
+        when a.ASS_HOUSE_NO_PREFIX in ( 'ABOVE' , 'OFF' , '(OFF)' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then ''
+        when a.ASS_HOUSE_NO_PREFIX like 'FACTORY%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 8))
+        when a.ASS_HOUSE_NO_PREFIX like 'FLAT%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 5))
+        when a.ASS_HOUSE_NO_PREFIX like 'HALL%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 5))
+        when a.ASS_HOUSE_NO_PREFIX like 'OFFICE%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 7))
+        when a.ASS_HOUSE_NO_PREFIX like 'SHED%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 5))
+        when a.ASS_HOUSE_NO_PREFIX like 'SHOP%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 5))
+        when a.ASS_HOUSE_NO_PREFIX like 'SILO%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 5))
+        when a.ASS_HOUSE_NO_PREFIX like 'SUITE%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 6))
+        when a.ASS_HOUSE_NO_PREFIX like 'UNIT%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 5))
+        when upper(substr(a.ASS_HOUSE_NO_PREFIX, -1)) between 'A' and 'Z' then substr(a.ASS_HOUSE_NO_PREFIX,1,length(a.ASS_HOUSE_NO_PREFIX)-1)
+        when a.ASS_HOUSE_NO_PREFIX like '%-%' then 'XXXX'
+        when a.ASS_HOUSE_NO_PREFIX like '%&%' then 'XXXX'
+        else ifnull ( a.ASS_HOUSE_NO_PREFIX , '' )
     end as blg_unit_id_1,
     case
-        when ASS_HOUSE_NO_PREFIX in ( 'ABOVE' , 'OFF' , '(OFF)' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then ''
-        when upper(substr(ASS_HOUSE_NO_PREFIX, -1)) between 'A' and 'Z' then upper(substr(ASS_HOUSE_NO_PREFIX, -1))
+        when a.ASS_HOUSE_NO_PREFIX in ( 'ABOVE' , 'OFF' , '(OFF)' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then ''
+        when upper(substr(a.ASS_HOUSE_NO_PREFIX, -1)) between 'A' and 'Z' then upper(substr(a.ASS_HOUSE_NO_PREFIX, -1))
         else ''
     end as blg_unit_suffix_1,
      '' as blg_unit_prefix_2,
     case
-        when ASS_HOUSE_NO_PREFIX like '%-%' then 'XXXX'
-        when ASS_HOUSE_NO_PREFIX like '%&%' then 'XXXX'
+        when a.ASS_HOUSE_NO_PREFIX like '%-%' then 'XXXX'
+        when a.ASS_HOUSE_NO_PREFIX like '%&%' then 'XXXX'
         else ''
     end as blg_unit_id_2,
     '' as blg_unit_suffix_2,
     case
-        when ASS_HOUSE_NO_PREFIX like 'GROUND%' then 'GND'
+        when a.ASS_HOUSE_NO_PREFIX like 'GROUND%' then 'GND'
         else ''
     end as floor_type,
-    '' as floor_prefix_1, 
+    '' as floor_prefix_1,
     case
-        when ASS_HOUSE_NO_PREFIX like 'LEVEL%' then ltrim(substr(ASS_HOUSE_NO_PREFIX, 6))
+        when a.ASS_HOUSE_NO_PREFIX like 'LEVEL%' then ltrim(substr(a.ASS_HOUSE_NO_PREFIX, 6))
         else ''
     end as floor_no_1,
     '' as floor_suffix_1,
@@ -91,22 +91,22 @@ select
     '' as building_name,
     '' as complex_name,
     case
-        when ASS_HOUSE_NO_PREFIX in ( 'OFF' , '(OFF)' ) then 'OFF'
-        when ASS_HOUSE_NO_PREFIX in ( 'ABOVE' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then ASS_HOUSE_NO_PREFIX
-        when ASS_HOUSE_NO_SUFFIX in ( 'OFF' , '(OFF)' ) then 'OFF'
-        when ASS_HOUSE_NO_SUFFIX in ( 'ABOVE' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then ASS_HOUSE_NO_SUFFIX
+        when a.ASS_HOUSE_NO_PREFIX in ( 'OFF' , '(OFF)' ) then 'OFF'
+        when a.ASS_HOUSE_NO_PREFIX in ( 'ABOVE' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then a.ASS_HOUSE_NO_PREFIX
+        when a.ASS_HOUSE_NO_SUFFIX in ( 'OFF' , '(OFF)' ) then 'OFF'
+        when a.ASS_HOUSE_NO_SUFFIX in ( 'ABOVE' , 'REAR' , 'UPPER' , 'UPSTAIRS' ) then a.ASS_HOUSE_NO_SUFFIX
         else ''
     end as location_descriptor,
     '' as house_prefix_1,
-    ifnull ( cast ( cast ( ASS_HOUSE_NUMBER as integer ) as varchar ) , '' ) as house_number_1,
+    ifnull ( cast ( cast ( a.ASS_HOUSE_NUMBER as integer ) as varchar ) , '' ) as house_number_1,
     case
-        when ASS_HOUSE_NO_SUFFIX like '-%' then ''
-        else ifnull ( ASS_HOUSE_NO_SUFFIX , '' )
+        when a.ASS_HOUSE_NO_SUFFIX like '-%' then ''
+        else ifnull ( a.ASS_HOUSE_NO_SUFFIX , '' )
     end as house_suffix_1,
-    '' as house_prefix_2,    
+    '' as house_prefix_2,
     case
-        when ASS_HOUSE_NO_SUFFIX like '-%' then substr ( ASS_HOUSE_NO_SUFFIX , 2 , 99 )
-        when ASS_HOUSE_NO_SUFFIX like '&%' then substr ( ASS_HOUSE_NO_SUFFIX , 2 , 99 )
+        when a.ASS_HOUSE_NO_SUFFIX like '-%' then substr ( a.ASS_HOUSE_NO_SUFFIX , 2 , 99 )
+        when a.ASS_HOUSE_NO_SUFFIX like '&%' then substr ( a.ASS_HOUSE_NO_SUFFIX , 2 , 99 )
         else ''
     end as house_number_2,
     '' as house_suffix_2,
@@ -181,7 +181,7 @@ select
     '' as outside_property,
     '310' as lga_code,
     '' as crefno,
-    ASS_ADDRESS2 as summary
+    a.ASS_ADDRESS2 as summary
 from
     fujitsu_pr_assessadd_view a,
     fujitsu_pr_street s,
