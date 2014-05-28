3d044014632c8391a59962fb54bbc2f03ec1a4ac
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-05-28 16:38:29 +1000

    Remove filtering and ordering

--- ~Shared/SQL/Audits/Audit - Council Parcel.sql
+++ ~Shared/SQL/Audits/Audit - Council Parcel.sql
@@ -48,6 +48,4 @@ select
     cp.*,
     ( select vp.geometry from pc_vicmap_parcel vp where vp.spi = cp.spi limit 1 ) as geometry
 from PC_Council_Parcel cp
-where spi <> ''
-order by spi_validity desc, ( case plan_number when '' then 'zzz' else plan_number end ) , parish_code, township_code, sec, lot_number, allotment
 
