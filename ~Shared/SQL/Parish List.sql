4333d7c3bdbccbbab68f19c811826b53e5f33c66
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-07-20 21:41:03 +1000

    Add townships to parish list

--- ~Shared/SQL/Parish List.sql
+++ ~Shared/SQL/Parish List.sql
@@ -1,11 +1,24 @@
 select
-    Parish.parish_name,
-    Parish.parish_code
+    Parish.parish_name name,
+    Parish.parish_code code
 from
     vmadmin_parish_polygon Parish,
     vmadmin_lga_polygon LGA
 where
     st_intersects ( LGA.geometry , Parish.geometry ) and
-    LGA.lga_code = 338
+    LGA.lga_code = 314
+
+union
+
+select
+    Township.township_name name,
+    Township.township_code code
+from
+    vmadmin_township_polygon Township,
+    vmadmin_lga_polygon LGA
+where
+    st_intersects ( LGA.geometry , Township.geometry ) and
+    LGA.lga_code = 314
+
 order by
-    Parish.parish_name
+    code
