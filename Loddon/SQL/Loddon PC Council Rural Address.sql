151281b59c3f5253d6b43428c4e1f419d7e7b1c2
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-08-14 09:40:28 +1000

    Populate rural address coords as MGA54

--- Loddon/SQL/Loddon PC Council Rural Address.sql
+++ Loddon/SQL/Loddon PC Council Rural Address.sql
@@ -9,8 +9,8 @@ set
             where
                 ra.prop_no = pc_council_property_address.propnum and
                 ra.rural_no = pc_council_property_address.house_number_1 ) , '' ) ,
-    easting =  ifnull ( ( select X ( geometry ) from lsc_rural_address ra where ra.prop_no = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.house_number_1 ) , '' ) ,
-    northing = ifnull ( ( select Y ( geometry ) from lsc_rural_address ra where ra.prop_no = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.house_number_1 ) , '' ) ,
+    easting =  ifnull ( ( select round ( X ( geometry ) , 0 ) from lsc_rural_address ra where ra.prop_no = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.house_number_1 ) , '' ) ,
+    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from lsc_rural_address ra where ra.prop_no = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.house_number_1 ) , '' ) ,
     datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from lsc_rural_address ra where ra.prop_no = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.house_number_1 ) , '' )
 where
     propnum in ( select prop_no from lsc_rural_address where prop_no <> '' and geometry is not null ) and
