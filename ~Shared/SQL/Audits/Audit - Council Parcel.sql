69836b73f4137ecac1ff41d08b4f8725333b79a0
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-12-05 12:31:34 +1100

    Change crefno check to count

--- ~Shared/SQL/Audits/Audit - Council Parcel.sql
+++ ~Shared/SQL/Audits/Audit - Council Parcel.sql
@@ -49,7 +49,7 @@ select
     end as suggested_spi,
     ifnull ( ( select num_parcels from pc_council_property_parcel_count cppc where cppc.propnum = cp.propnum ) , 0 ) as propnum_in_council,
     ifnull ( ( select num_parcels from pc_vicmap_property_parcel_count vppc where vppc.propnum = cp.propnum ) , 0 ) as propnum_in_vicmap,
-    ifnull ( ( select crefno from pc_vicmap_parcel vp where vp.spi = cp.spi and length ( vp.spi ) >= 5 limit 1 ) , '' ) as vicmap_crefno,
+    ifnull ( ( select count(*) from ( select distinct spi from pc_vicmap_parcel vp where vp.crefno = cp.crefno and vp.crefno <> '' ) ) , 0 ) as crefno_in_vicmap,
     ifnull ( ( select edit_code from M1 where ( m1.spi = cp.spi and length ( cp.spi ) >= 5 ) or m1.propnum = cp.propnum limit 1 ) , '' ) as m1_edit_code,
     ifnull ( ( select comments from M1 where ( m1.spi = cp.spi and length ( cp.spi ) >= 5 ) or m1.propnum = cp.propnum limit 1 ) , '' ) as m1_comments,
     cp.*,
