df897af9c95182e8bcb0ebb6578b4c63e876856b
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-12-09 12:12:51 +1100

    Add warning for conflicting localities

--- ~Shared/SQL/M1 P Edits.sql
+++ ~Shared/SQL/M1 P Edits.sql
@@ -75,7 +75,11 @@ select
         end ||
         ' (' || ( select ezi_address from pc_vicmap_property_address where property_pfi = vp.property_pfi and is_primary <> 'N' limit 1 ) || ') with ' ||
         cp.propnum ||
-        ifnull ( ' (' || ( select cpa.ezi_address from pc_council_property_address cpa where cpa.propnum = cp.propnum and is_primary <> 'N' limit 1 ) , '' ) || ')' as comments,
+        ifnull ( ' (' || ( select cpa.ezi_address from pc_council_property_address cpa where cpa.propnum = cp.propnum and is_primary <> 'N' limit 1 ) , '' ) || ')' ||
+    case
+        when ( select locality_name from pc_vicmap_property_address where property_pfi = vp.property_pfi ) <> ( select locality_name from pc_council_property_address where propnum = cp.propnum ) then ' (**WARNING**: conflicting localities)'
+        else ''
+    end as comments,
     centroid ( vp.geometry ) as geometry
 from
     pc_vicmap_parcel vp,
