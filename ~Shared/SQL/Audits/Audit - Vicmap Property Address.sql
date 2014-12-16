23e7cdbb46f8b90e6002fcaacd03cebb157dbb83
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2014-12-16 14:32:44 +1100

    Add Vicmap Property Address audit

---
+++ ~Shared/SQL/Audits/Audit - Vicmap Property Address.sql
@@ -0,0 +1,14 @@
+select
+    property_pfi,
+    propnum,    
+    multi_assessment,
+    status,    
+    address_pfi,    
+    ezi_address,
+    ifnull ( ( select edit_code from M1 where m1.propnum = vpa.propnum and vpa.propnum <> '' limit 1 ) , '' ) as m1_edit_code,
+    ifnull ( ( select comments from M1 where m1.propnum = vpa.propnum and vpa.propnum <> '' limit 1 ) , '' ) as m1_comments,    
+    area ( st_transform ( geometry , 3111 ) ) property_area,
+    area ( envelope ( st_transform ( geometry , 3111 ) ) ) property_mbr_area,
+    area ( geometry ) / area ( envelope ( geometry ) ) cohesion,
+    geometry as geometry
+from pc_vicmap_property_address vpa
