77bb96f70b3b33acd6abc07ea6d091f8e5b11e1d
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2015-04-14 17:45:47 +1000

    Commence Benalla config with blank query template

---
+++ Benalla/SQL/Benalla PC Council Parcel.sql
@@ -0,0 +1,58 @@
+select
+    *,
+    case
+        when plan_number <> '' and lot_number = '' then plan_number
+        when plan_number <> '' and sec <> '' then lot_number || '~' || sec || '\' || plan_number
+        when plan_number <> '' and block <> '' then lot_number || '~' || block || '\' || plan_number
+        when plan_number <> '' then lot_number || '\' || plan_number
+        when ( parish_code <> '' or township_code <> '' ) then
+            subdivision ||
+            case when subdivision <> '' and ( portion <> '' or allotment <> '' ) then '~' else '' end ||
+            portion ||
+            case when portion <> '' and allotment <> '' then '~' else '' end ||
+            allotment ||
+            case when sec <> '' then '~' else '' end ||
+            sec ||
+            '\PP' ||
+            case when township_code <> '' then township_code else parish_code end
+        else ''
+    end as spi,
+    case
+        when plan_numeral <> '' and lot_number = '' then plan_numeral
+        when plan_number <> '' and sec <> '' then lot_number || '~' || sec || '\' || plan_numeral
+        when plan_number <> '' and block <> '' then lot_number || '~' || block || '\' || plan_numeral
+        when plan_numeral <> '' then lot_number || '\' || plan_numeral
+        when ( parish_code <> '' or township_code <> '' ) then
+            subdivision ||
+            case when subdivision <> '' and ( portion <> '' or allotment <> '' ) then '~' else '' end ||
+            portion ||
+            case when portion <> '' and allotment <> '' then '~' else '' end ||
+            allotment ||
+            case when sec <> '' then '~' else '' end ||
+            sec ||
+            '\' ||
+            case when township_code <> '' then township_code else parish_code end
+        else ''
+    end as simple_spi
+from
+(
+select distinct
+    '' as propnum,
+    '' as status,
+    '' as crefno,
+    '' as part,
+    '' as summary,
+    '' as plan_number,
+    '' as plan_prefix,
+    '' as plan_numeral,
+    '' as lot_number,
+    '' as allotment,
+    '' as sec,
+    '' as block,
+    '' as portion,
+    '' as subdivision,
+    '' as parish_code,
+    '' as township_code,
+    '' as lga_code,
+    '' as assnum
+)
---
+++ Benalla/SQL/Benalla PC Council Property Address.sql
@@ -0,0 +1,75 @@
+select
+    *,
+    ltrim ( num_road_address ||
+        rtrim ( ' ' || locality_name ) ) as ezi_address
+from (
+
+select
+    *,
+    ltrim ( road_name_combined ||
+        rtrim ( ' ' || locality_name ) ) as road_locality,
+    ltrim ( num_address ||
+        rtrim ( ' ' || road_name_combined ) ) as num_road_address
+from (
+
+select
+    *,
+    blg_unit_prefix_1 || blg_unit_id_1 || blg_unit_suffix_1 ||
+        case when ( blg_unit_id_2 <> '' or blg_unit_suffix_2 <> '' ) then '-' else '' end ||
+        blg_unit_prefix_2 || blg_unit_id_2 || blg_unit_suffix_2 ||
+        case when ( blg_unit_id_1 <> '' or blg_unit_suffix_1 <> '' ) then '/' else '' end ||
+        house_prefix_1 || house_number_1 || house_suffix_1 ||
+        case when ( house_number_2 <> '' or house_suffix_2 <> '' ) then '-' else '' end ||
+        house_prefix_2 || house_number_2 || house_suffix_2 as num_address,
+    ltrim ( road_name ||
+        rtrim ( ' ' || road_type ) ||
+        rtrim ( ' ' || road_suffix ) ) as road_name_combined
+from (
+
+select
+    '' as propnum,
+    '' as status,
+    '' as base_propnum,
+    '' as is_primary,
+    '' as distance_related_flag,
+    '' as hsa_flag,
+    '' as hsa_unit_id,
+    '' as blg_unit_type,
+    '' as blg_unit_prefix_1,
+    '' as blg_unit_id_1,
+    '' as blg_unit_suffix_1,
+    '' as blg_unit_prefix_2,
+    '' as blg_unit_id_2,
+    '' as blg_unit_suffix_2,
+    '' as floor_type,
+    '' as floor_prefix_1,
+    '' as floor_no_1,
+    '' as floor_suffix_1,
+    '' as floor_prefix_2,
+    '' as floor_no_2,
+    '' as floor_suffix_2,
+    '' as building_name,
+    '' as complex_name,
+    '' as location_descriptor,
+    '' as house_prefix_1,
+    '' as house_number_1,
+    '' as house_suffix_1,
+    '' as house_prefix_2,
+    '' as house_number_2,
+    '' as house_suffix_2,
+    '' as road_name, 
+    '' as road_type,
+    '' as road_suffix,
+    '' as locality_name,
+    '' as postcode,
+    '' as access_type,
+    '' as easting,
+    '' as northing,
+    '' as datum_proj,
+    '' as outside_property,
+    '' as lga_code,
+    '' as crefno,
+    '' as summary
+)
+)
+)
