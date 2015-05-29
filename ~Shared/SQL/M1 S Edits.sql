pozi/PoziConnectConfig

commit fc42d29990584d179f75bc9f8a395b5ec47cbe96

Prevent road name warning caused by hyphens

~Shared/SQL/M1 S Edits.sql
@@ -72,7 +72,7 @@ select distinct
             else ''
         end ||
         case
-            when cpa.road_locality not in ( select road_locality from pc_vicmap_property_address ) then ' (**WARNING**: new road name)'
+            when replace ( cpa.road_locality , '-' , ' ' ) not in ( select replace ( cpa.road_locality , '-' , ' ' ) from pc_vicmap_property_address ) then ' (**WARNING**: new road name)'
             else ''
         end as comments,
     centroid ( ( select vpa.geometry from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum and cpa.is_primary <> 'N' limit 1 ) ) as geometry

Mitchell/SQL/Mitchell PC Council Parcel.sql
@@ -38,7 +38,7 @@ from
 (
 select
     cast ( P.property_no as varchar ) as propnum,
-    cast ( L.land_no as varchar ) as crefno,
+    '' as crefno,
     ifnull ( substr ( P.override_legal_description , 1 , 99 ) , '' ) as summary,
     case P.status
         when 'F' then 'P'
@@ -49,25 +49,25 @@ select
         else ''
     end as part,
     case
-        when L.plan_desc in ( 'CA' , 'PTCA' ) then ''
+        when L.plan_desc in ( 'CA' , 'PTCA' , 'PT CA' ) then ''
         when substr ( trim ( ifnull ( L.plan_no , '' ) ) , -1 ) in ( '1','2','3','4','5','6','7','8','9','0' ) then trim ( ifnull ( L.plan_desc , '' ) ) || ifnull ( L.plan_no , '' )
         else trim ( ifnull ( L.plan_desc , '' ) ) || substr ( trim ( ifnull ( L.plan_no , '' ) ) , 1 , length ( trim ( ifnull ( L.plan_no , '' ) ) ) - 1 )
     end as plan_number,
     case
-        when L.plan_desc in ( 'CA' , 'PTCA' ) then ''
-        else ifnull ( L.plan_desc , '' ) 
+        when L.plan_desc in ( 'CA' , 'PTCA' , 'PT CA' ) then ''
+        else ifnull ( L.plan_desc , '' )
     end as plan_prefix,
     case
         when substr ( trim ( L.plan_no ) , -1 ) in ( '1','2','3','4','5','6','7','8','9','0' ) then L.plan_no
         else ifnull ( substr ( trim ( L.plan_no ) , 1 , length ( trim ( L.plan_no ) ) - 1 ) , '' )
     end as plan_numeral,
     case
-        when L.plan_desc in ( 'CA' , 'PTCA' ) then ''
+        when L.plan_desc in ( 'CA' , 'PTCA' , 'PT CA' ) then ''
         else upper ( replace ( ifnull ( L.lot , '' ) , ' ' , '' ) ) 
     end as lot_number,
     case
-        when L.plan_desc in ( 'CA' , 'PTCA' ) then L.lot
-        when L.plan_desc is null and L.plan_no = '' then L.lot
+        when L.plan_desc in ( 'CA' , 'PTCA' , 'PT CA' ) then ifnull ( L.text10 , '' )
+        when L.plan_desc is null and L.plan_no = '' then ifnull ( L.text10 , '' )
         else ''
     end as allotment,
     ifnull ( L.parish_section , '' ) as sec,
@@ -75,19 +75,51 @@ select
     ifnull ( L.parish_portion , '' ) as portion,
     '' as subdivision,
     case L.parish_desc
-        when 'MEE' then '3078'
-        when 'MEP' then '3087'
-        when 'PUR' then '3426'
-        when 'TAL' then '3530'
-        when 'WAN' then '3729'
-        when 'YAN' then '3950'
+        when 'BAYNTON' then '2094'
+        when 'BROADFORD' then '2235'
+        when 'BYLANDS' then '2318'
+        when 'CLONBINANE' then '2389'
+        when 'DERRIL' then '2515'
+        when 'DWEIT GUIM' then '2496'
+        when 'FLOWERDALE' then '2614'
+        when 'FORBES' then '2615'
+        when 'GHIN GHIN' then '2651'
+        when 'GLENAROUA' then '2665'
+        when 'GLENBURNIE' then '2667'
+        when 'GLENHOPE' then '2675'
+        when 'GOLDIE' then '2694'
+        when 'HEATHCOTE' then '2750'
+        when 'KERRISDALE' then '2866'
+        when 'KOBYBOYN' then '2894'
+        when 'LANGLEY' then '2970'
+        when 'LOWRY' then '3018'
+        when 'MANGALORE' then '3053'
+        when 'MERRIANG' then '3093'
+        when 'MITCHELL' then '3125'
+        when 'MORANDING' then '3182'
+        when 'NORTHWOOD' then '3324'
+        when 'PANYULE' then '3370'
+        when 'PUCKAPUNYAL' then '3420'
+        when 'PYALONG' then '3430'
+        when 'SEYMOUR' then '3481'
+        when 'SPRING PLAINS' then '3496'
+        when 'TALLAROOK' then '3532'
+        when 'TOOBORAC' then '3611'
+        when 'TRAAWOOL' then '3645'
+        when 'WALLAN WALLAN' then '3707'
+        when 'WARROWITUE' then '3766'
+        when 'WILLOWMAVIN' then '3828'
+        when 'WINDHAM' then '3834'
+        when 'WORROUGH' then '3903'
+        when 'PUCKA' then '3420'
+        when 'SPRING PL' then '3496'
+        when 'WILLOWMAV' then '3828'
+        when 'WALLAN WAL' then '3707'
         else ''
     end as parish_code,
-    case L.county_desc
-        when 'BUSH' then '5139'
-        when 'DENN' then '5235'
-        when 'WARR' then '5841'
-        when 'WOOD' then '5875'
+    case L.parish_desc
+        when 'KILMORE' then '5420'
+        when 'MERINGO TN' then '5518'
         else ''
     end as township_code,
     '346' as lga_code

commit 926091326b2bd600796a4b6cf394443d9ecfcd91

Remove faulty table alias
The wrong table prefix caused the S edit query to be very slow. Testing
at Knox showed that fixing this results in the query only taking one
quarter of the time.

The bug was introduced in
https://github.com/groundtruth/PoziConnectConfig/commit/fc42d29990584d179f75bc9f8a395b5ec47cbe96

~Shared/SQL/M1 S Edits.sql
@@ -72,7 +72,7 @@ select distinct
             else ''
         end ||
         case
-            when replace ( cpa.road_locality , '-' , ' ' ) not in ( select replace ( cpa.road_locality , '-' , ' ' ) from pc_vicmap_property_address ) then ' (**WARNING**: new road name)'
+            when replace ( cpa.road_locality , '-' , ' ' ) not in ( select replace ( road_locality , '-' , ' ' ) from pc_vicmap_property_address ) then ' (**WARNING**: new road name)'
             else ''
         end as comments,
     centroid ( ( select vpa.geometry from pc_vicmap_property_address vpa where vpa.propnum = cpa.propnum and cpa.is_primary <> 'N' limit 1 ) ) as geometry