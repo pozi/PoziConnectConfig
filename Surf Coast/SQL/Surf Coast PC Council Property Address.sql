e0737a59553f6447e1c974da232c70cff639b163
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2015-11-19 08:40:48 +1100

    Substitute ampersands

--- Surf Coast/SQL/Surf Coast PC Council Property Address.sql
+++ Surf Coast/SQL/Surf Coast PC Council Property Address.sql
@@ -61,7 +61,7 @@ select distinct
     '' as house_prefix_2,
     ifnull ( cast ( auprstad.hou_end as varchar ) , '' ) as house_number_2,
     ifnull ( upper ( auprstad.end_alp ) , '' ) as house_suffix_2,
-    upper ( auprstad.str_nme ) as road_name,
+    upper ( replace ( auprstad.str_nme , '&' , 'AND' ) ) as road_name,
     case
         when auprstad.str_typ in ( 'AVEN' , 'AVES' , 'AVEE' , 'AVEW' , 'AVEX' ) THEN 'AVENUE'
         when auprstad.str_typ in ( 'RDN' , 'RDS' , 'RDE' , 'RDW' , 'RDX' ) THEN 'ROAD'
