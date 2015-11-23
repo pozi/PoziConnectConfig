bef103d7c20248d296ce9f0d4630bf2e93ffee73
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2015-11-23 14:09:48 +1100

    Populate outside_property

--- South Gippsland/SQL/South Gippsland PC Council Rural Address.sql
+++ South Gippsland/SQL/South Gippsland PC Council Rural Address.sql
@@ -11,7 +11,8 @@ set
                 ra.house = cast ( pc_council_property_address.house_number_1 as integer )  , '' ) ,
     easting = ifnull ( ( select round ( X ( geometry ) , 0 ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' ) ,
     northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' ) ,
-    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' )
+    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' ) ,
+    outside_property = ifnull ( ( select ra.outside from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' )
 where
     propnum in ( select prop_key from sgsc_rural_address where prop_key <> '' and geometry is not null ) and
     is_primary <> 'N'
