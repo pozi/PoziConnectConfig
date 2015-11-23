45c302e749f37b15a635155bb50400565e3d671f
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2015-11-23 11:27:06 +1100

    Cater for house field being integer

--- South Gippsland/SQL/South Gippsland PC Council Rural Address.sql
+++ South Gippsland/SQL/South Gippsland PC Council Rural Address.sql
@@ -8,10 +8,10 @@ set
             end from sgsc_rural_address ra
             where
                 ra.prop_key = pc_council_property_address.propnum and
-                ra.house = pc_council_property_address.house_number_1 ) , '' ) ,
-    easting =  ifnull ( ( select round ( X ( geometry ) , 0 ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = pc_council_property_address.house_number_1 ) , '' ) ,
-    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = pc_council_property_address.house_number_1 ) , '' ) ,
-    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = pc_council_property_address.house_number_1 ) , '' )
+                ra.house = cast ( pc_council_property_address.house_number_1 as integer )  , '' ) ,
+    easting = ifnull ( ( select round ( X ( geometry ) , 0 ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' ) ,
+    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' ) ,
+    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from sgsc_rural_address ra where ra.prop_key = pc_council_property_address.propnum and ra.house = cast ( pc_council_property_address.house_number_1 as integer ) ) , '' )
 where
     propnum in ( select prop_key from sgsc_rural_address where prop_key <> '' and geometry is not null ) and
     is_primary <> 'N'
