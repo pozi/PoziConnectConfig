f2f5e6eb1409d2fedb973a1bec161ead846c5605
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2016-05-30 16:11:27 +1000

    Implement Wellington rural addressing

---
+++ Wellington/SQL/Wellington PC Council Rural Address.sql
@@ -0,0 +1,8 @@
+update pc_council_property_address
+set
+    easting = ifnull ( ( select round ( X ( geometry ) , 0 ) from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
+    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
+    datum_proj = ifnull ( ( select 'EPSG:28355' from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
+    is_primary = ifnull ( ( select case when primary_ = -1 then 'Y' else 'N' end from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' )
+where
+    propnum in ( select assessment from wsc_rural_address ra where ra.assessment <> '' )
