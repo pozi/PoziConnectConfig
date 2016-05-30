e34d881cf29d2713da29e5e9caebd3a717431f9e
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2016-05-30 16:34:51 +1000

    Add dist_rel and outside_property fields

--- Wellington/SQL/Wellington PC Council Rural Address.sql
+++ Wellington/SQL/Wellington PC Council Rural Address.sql
@@ -1,8 +1,10 @@
 update pc_council_property_address
 set
+    distance_related_flag = ifnull ( ( select 'Y' from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
     easting = ifnull ( ( select round ( X ( geometry ) , 0 ) from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
     northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
     datum_proj = ifnull ( ( select 'EPSG:28355' from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
-    is_primary = ifnull ( ( select case when primary_ = -1 then 'Y' else 'N' end from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' )
+    is_primary = ifnull ( ( select case when primary_ = -1 then 'Y' else 'N' end from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' ) ,
+    outside_property = ifnull ( ( select case when ospolyflag = -1 then 'Y' else 'N' end from wsc_rural_address ra where ra.assessment = pc_council_property_address.propnum and ra.rural_no = pc_council_property_address.num_address ) , '' )
 where
     propnum in ( select assessment from wsc_rural_address ra where ra.assessment <> '' )
