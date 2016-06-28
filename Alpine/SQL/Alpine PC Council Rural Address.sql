2ee2b6bcbf6075dbcb30a14c38e09f9948983708
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2016-06-29 09:37:14 +1000

    Implement Alpine rural address integration

---
+++ Alpine/SQL/Alpine PC Council Rural Address.sql
@@ -0,0 +1,10 @@
+update pc_council_property_address
+set
+    distance_related_flag = ifnull ( ( select 'Y' from asc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    easting = ifnull ( ( select cast ( round ( ra.easting , 0 ) as integer ) from asc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    northing = ifnull ( ( select cast ( round ( ra.northing , 0 ) as integer ) from asc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    datum_proj = ifnull ( ( select ra.datum_proj from asc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    outside_property = ifnull ( ( select ra.outside_property from asc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    is_primary = ifnull ( ( select ra.is_primary from asc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' )
+where
+    propnum in ( select propnum from asc_rural_address ra where ra.propnum <> '' )
