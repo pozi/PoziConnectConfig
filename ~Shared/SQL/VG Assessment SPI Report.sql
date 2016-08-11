c475d090d645c9060a944886ca469023834ae7ef
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2016-08-11 15:45:50 +1000

    Add parcel area output to VG report

--- ~Shared/SQL/VG Assessment SPI Report.sql
+++ ~Shared/SQL/VG Assessment SPI Report.sql
@@ -1,9 +1,17 @@
 select distinct
     assnum,
     crefno,
-    spi
+    spi,
+    case
+        when spi <> '' then ifnull ( ( select cast ( round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) as integer ) from pc_vicmap_parcel vp where vp.spi = cp.spi ) , '' )
+        else ''
+    end as vicmap_parcel_area_sqm,
+    case
+        when spi <> '' then ifnull ( ( select round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) / 10000 from pc_vicmap_parcel vp where vp.spi = cp.spi ) , '' )
+        else ''
+    end as vicmap_parcel_area_ha
 from
-    pc_council_parcel
+    pc_council_parcel cp
 where
     assnum <> ''
 order by
