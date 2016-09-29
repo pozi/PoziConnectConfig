89b4cac388ab3afae6272299ee1d7d6d0d80cfae
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2016-09-30 09:09:26 +1000

    Remove alt_spi_in_council calculation
    which was resulting in long processing times

--- ~Shared/SQL/Audits/Audit - Vicmap Parcel.sql
+++ ~Shared/SQL/Audits/Audit - Vicmap Parcel.sql
@@ -11,7 +11,6 @@ select
     ( select count(*) from pc_vicmap_parcel vpx where vpx.spi = vp.spi and length ( vp.spi ) >= 5 ) as spi_in_vicmap,
     ifnull ( ( select group_concat ( propnum , ';' ) from ( select propnum from pc_vicmap_parcel vpx where vpx.spi = vp.spi and length ( vp.spi ) >= 5 limit 20 ) ) , '' ) as vicmap_propnums,
     ( select count(*) from pc_council_parcel cp where cp.simple_spi = vp.simple_spi and cp.spi <> vp.spi and length ( vp.spi ) >= 5 ) as partial_spi_in_council,
-    ( select count(*) from pc_council_parcel cp where cp.spi = vp.further_description and length ( cp.spi ) >= 5 ) as alt_spi_in_council,
     ifnull ( ( select num_parcels from pc_council_property_parcel_count cppc where cppc.propnum = vp.propnum ) , 0 ) as propnum_in_council,
     ifnull ( ( select num_parcels from pc_vicmap_property_parcel_count vppc where vppc.propnum = vp.propnum ) , 0 ) as propnum_in_vicmap,
     ifnull ( ( select crefno from pc_council_parcel cp where cp.spi = vp.spi and length ( cp.spi ) >= 5 ) , '' ) as council_crefno,
