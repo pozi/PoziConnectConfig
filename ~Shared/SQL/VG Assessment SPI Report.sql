93b276b650da4604c00c5e6ef5749f915d78b78b
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2017-03-17 16:24:06 +1100

    Use crefno to match to Vicmap parcel if spi isn't valid

--- ~Shared/SQL/VG Assessment SPI Report.sql
+++ ~Shared/SQL/VG Assessment SPI Report.sql
@@ -3,11 +3,13 @@ select distinct
     crefno,
     spi,
     case
-        when spi <> '' then ifnull ( ( select cast ( round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) as integer ) from pc_vicmap_parcel vp where vp.spi = cp.spi ) , '' )
+        when spi <> '' and cp.spi in ( select spi from pc_vicmap_parcel ) then ifnull ( ( select cast ( round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) as integer ) from pc_vicmap_parcel vp where vp.spi = cp.spi ) , '' )
+        when crefno <> '' then ifnull ( ( select cast ( round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) as integer ) from pc_vicmap_parcel vp where vp.crefno = cp.crefno ) , '' )
         else ''
     end as vicmap_parcel_area_sqm,
     case
-        when spi <> '' then ifnull ( ( select round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) / 10000 from pc_vicmap_parcel vp where vp.spi = cp.spi ) , '' )
+        when spi <> '' and cp.spi in ( select spi from pc_vicmap_parcel ) then ifnull ( ( select round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) / 10000 from pc_vicmap_parcel vp where vp.spi = cp.spi ) , '' )
+        when crefno <> '' then ifnull ( ( select round ( st_area ( st_transform ( vp.geometry , 3111 ) ) ) / 10000 from pc_vicmap_parcel vp where vp.crefno = cp.crefno ) , '' )
         else ''
     end as vicmap_parcel_area_ha
 from
