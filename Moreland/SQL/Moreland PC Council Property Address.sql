e0cd9696956362e904468279422be74f7e8b9fe0
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2017-04-18 15:39:48 +1000

    Moreland: cater for street names with apostrophes

--- Moreland/SQL/Moreland PC Council Property Address.sql
+++ Moreland/SQL/Moreland PC Council Property Address.sql
@@ -106,7 +106,7 @@ select
         when lpaaddr.endhoussfx = '0' or lpaaddr.endhoussfx is null then ''
         else cast ( lpaaddr.endhoussfx as varchar )
     end as house_suffix_2,
-    upper ( cnacomp.descr ) as road_name,
+    upper ( replace ( cnacomp.descr , '''' , '' ) ) as road_name,
     case
         when
             cnaqual.descr like '% NORTH' or
