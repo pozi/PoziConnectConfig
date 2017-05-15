d15c0ee82f1617e273ad7af3e2564155321a5865
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2017-05-15 11:54:40 +1000

    Moorabool: include both internal and constructed spi values

--- Moorabool/SQL/Moorabool PC Council Parcel.sql
+++ Moorabool/SQL/Moorabool PC Council Parcel.sql
@@ -7,6 +7,17 @@ select
     *,
     case
         when internal_spi <> '' then internal_spi
+        else constructed_spi
+    end as spi,
+    case
+        when internal_spi <> '' then 'council_spi'
+        else 'council_attributes'
+    end as spi_source
+from
+(
+select
+    *,
+    case
         when plan_number <> '' and lot_number = '' then plan_number
         when plan_number <> '' and sec <> '' then lot_number || '~' || sec || '\' || plan_number
         when plan_number <> '' and block <> '' then lot_number || '~' || block || '\' || plan_number
@@ -22,7 +33,7 @@ select
             '\PP' ||
             case when township_code <> '' then township_code else parish_code end
         else ''
-    end as spi
+    end as constructed_spi
 from
 (
 select
@@ -164,5 +175,6 @@ where
     Parcel.Status = 'Active' and
     Parcel.Ended is null and
     Property.Type not in ( 672 , 700 )
+)
 )
 )
