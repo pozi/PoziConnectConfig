7dfd41f1d9c8dc7e59b6045737e09bbcc7e31d1c
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2017-11-14 11:55:14 +1100

    Remove transform function to improve query performance

--- ~Shared/SQL/Audits/Audit - Vicmap Property Address.sql
+++ ~Shared/SQL/Audits/Audit - Vicmap Property Address.sql
@@ -8,8 +8,8 @@ select
     ezi_address,
     ifnull ( ( select edit_code from m1 where m1.propnum = vpa.propnum and vpa.propnum <> '' limit 1 ) , '' ) as m1_edit_code,
     ifnull ( ( select comments from m1 where m1.propnum = vpa.propnum and vpa.propnum <> '' limit 1 ) , '' ) as m1_comments,
-    area ( st_transform ( geometry , 3111 ) ) property_area,
-    area ( envelope ( st_transform ( geometry , 3111 ) ) ) property_mbr_area,
+    area ( geometry ) property_area,
+    area ( envelope ( geometry ) ) property_mbr_area,
     area ( geometry ) / area ( envelope ( geometry ) ) cohesion,
     geometry as geometry
 from pc_vicmap_property_address vpa
