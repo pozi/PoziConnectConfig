fa46c77a25d8b4df7aa7fc1c8f0ef64994302ee0
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2018-06-08 15:48:41 +1000

    S edits: detect new hotel style addresses

--- ~Shared/SQL/M1 S Edits.md
+++ ~Shared/SQL/M1 S Edits.md
@@ -67,12 +67,12 @@ Include only properties that 1) already exist in Vicmap; 2) will appear in a P e
   propnum in ( select propnum from m1_a_edits ) )
 ```
 
-Exclude properties where the only difference between the Council and Vicmap address is a hyphen or an apostrophe.
+Include only properties where there is a difference between the Council and Vicmap address (apart from a hyphen or an apostrophe), or if it's a new hotel style address.
 
-```sql
-not replace ( replace ( cpa.num_road_address , '-' , ' ' ) , '''' , '' ) = ifnull ( replace ( replace ( vpa.num_road_address , '-' , ' ' ) , '''' , '' ) , '' )
 ```
-
+( not replace ( replace ( cpa.num_road_address , '-' , ' ' ) , '''' , '' ) = ifnull ( replace ( replace ( vpa.num_road_address , '-' , ' ' ) , '''' , '' ) , '' ) or
+  ( cpa.hsa_flag = 'Y' and vpa.hsa_flag = 'N' ) )
+```
 Include only properties where the council address has a valid road name.
 
 ```sql
--- ~Shared/SQL/M1 S Edits.sql
+++ ~Shared/SQL/M1 S Edits.sql
@@ -104,6 +104,10 @@ select
             else ''
         end ||
         case
+            when cpa.hsa_flag = 'Y' and vpa.hsa_flag = 'N' then ' (**NOTE**: new hotel style address)'
+            else ''
+        end ||
+        case
             when length ( cpa.building_name ) > 45 then ' (**WARNING**: building name exceeds limit of 45 characters)'
             else ''
         end ||
@@ -123,7 +127,8 @@ where
     cpa.propnum not in ( select vpax.propnum from pc_vicmap_property_address vpax, m1_r_edits r where vpax.property_pfi = r.property_pfi ) and
     ( cpa.propnum in ( select propnum from pc_vicmap_property_address ) or
       cpa.propnum in ( select propnum from m1_p_edits ) ) and
-    not replace ( replace ( cpa.num_road_address , '-' , ' ' ) , '''' , '' ) = ifnull ( replace ( replace ( vpa.num_road_address , '-' , ' ' ) , '''' , '' ) , '' ) and
+    ( not replace ( replace ( cpa.num_road_address , '-' , ' ' ) , '''' , '' ) = ifnull ( replace ( replace ( vpa.num_road_address , '-' , ' ' ) , '''' , '' ) , '' ) or
+      ( cpa.hsa_flag = 'Y' and vpa.hsa_flag = 'N' ) ) and
     not replace ( cpa.num_road_address , 'MT ' , 'MOUNT ' ) = ifnull ( replace ( vpa.num_road_address , 'MT ' , 'MOUNT ' ) , '' ) and
     cpa.road_name <> '' and
     not ( select count(*) from ( select distinct ezi_address from pc_vicmap_property_address vpax where vpax.propnum = cpa.propnum ) ) > 1
