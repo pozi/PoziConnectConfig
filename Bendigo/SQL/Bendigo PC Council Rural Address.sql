1942c8f55608478b684ee8658f35a935906148e6
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2019-02-22 09:44:59 +1100

    Bendigo: fix rural address query

--- Bendigo/SQL/Bendigo PC Council Rural Address.sql
+++ Bendigo/SQL/Bendigo PC Council Rural Address.sql
@@ -1,10 +1,10 @@
 update pc_council_property_address
 set
-    is_primary = ifnull ( ( select replace ( replace ( upper ( ra."is_primary" ) , 'T' , 'Y' ) , 'F' , 'N' ) from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    distance_related_flag =  ifnull ( ( select 'Y' from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    is_primary = ifnull ( ( select is_primary from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    distance_related_flag =  ifnull ( ( select dist_related_flag from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
     easting =  ifnull ( ( select round ( X ( geometry ) , 0 ) from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
     northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
     datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    outside_property = ifnull ( ( select replace ( replace ( upper ( ra."outside_pr" ) , 'T' , 'Y' ) , 'F' , 'N' ) from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' )
+    outside_property =  ifnull ( ( select outside_property from cogb_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) 
 where
     propnum in ( select propnum from cogb_rural_address where geometry is not null )
