7df82e45028370cc7ff321ca9d8fe3d4cfb1bde5
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2019-07-25 11:31:01 +1000

    South Gippsland: use council's internal SPI

--- South Gippsland/SQL/South Gippsland PC Council Parcel.sql
+++ South Gippsland/SQL/South Gippsland PC Council Parcel.sql
@@ -6,6 +6,18 @@ from
 select
     *,
     case
+        when internal_spi <> '' then internal_spi
+        else constructed_spi
+    end as spi,
+    case
+        when internal_spi <> '' then 'council_spi'
+        else 'council_attributes'
+    end as spi_source
+from
+(
+select
+    *,
+    case
         when plan_number <> '' and lot_number = '' then plan_number
         when plan_number <> '' and sec <> '' then lot_number || '~' || sec || '\' || plan_number
         when plan_number <> '' and block <> '' then lot_number || '~' || block || '\' || plan_number
@@ -21,7 +33,7 @@ select
             '\PP' ||
             case when township_code <> '' then township_code else parish_code end
         else ''
-    end as spi
+    end as constructed_spi
 from
 (
 select distinct
@@ -31,6 +43,7 @@ select distinct
         when 'A' then 'P'
     end as status,
     cast ( lpaparc.tpklpaparc as varchar ) as crefno,
+    lpaparc.spi as internal_spi,
     ifnull ( lpaparc.fmtparcel , '' ) as summary,
     case
         when lpaparc.plannum is null then cast (null as varchar)
@@ -153,5 +166,6 @@ where
     lpaprop.tfklpacncl = 13 and
     ifnull ( lpaparc.plancode , '' ) <> 'AG' and
     ifnull ( lpaparc.parcelcode , '' ) not in ( 'VR' , 'E' )
+)
 )
 )
