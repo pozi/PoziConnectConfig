e980266e1a9146d62b69851633b423db446b208f
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2020-01-24 09:27:31 +1100

    Separate property and parcel values from map links

--- ~Shared/SQL/Audits/Internal SPI vs Constructed SPI.sql
+++ ~Shared/SQL/Audits/Internal SPI vs Constructed SPI.sql
@@ -1,8 +1,9 @@
 select
+    propnum,
     case
-        when propnum in ( select propnum from pc_vicmap_property_address ) then '=HYPERLINK("https://vicmap.pozi.com/?propertypfi=' || ( select property_pfi from pc_vicmap_property_address vpa where vpa.propnum = x.propnum ) || '" , "' || propnum || '")'
-        else propnum
-    end as propnum,
+        when propnum in ( select propnum from pc_vicmap_property_address ) then '=HYPERLINK("https://vicmap.pozi.com/?propertypfi=' || ( select property_pfi from pc_vicmap_property_address vpa where vpa.propnum = x.propnum ) || '" , "' || propnum || ' map' || '")'
+        else ''
+    end as property_map,
     ifnull ( ( select ezi_address from pc_vicmap_property_address where propnum = x.propnum ) , '' ) as address,
     case
         when propnum in ( select propnum from pc_vicmap_property_address ) or propnum in ( select propnum from m1 ) then 'Y'
@@ -13,21 +14,24 @@ select
         when internal_spi = constructed_spi then 'Y'
         else 'N'
     end as spis_match,
+    internal_spi,
     case
-        when internal_spi in ( select spi from pc_vicmap_parcel where spi <> '' ) then '=HYPERLINK("https://vicmap.pozi.com/?parcelspi=' || internal_spi || '" , "' || internal_spi || '")'
-        else internal_spi
-    end as internal_spi,
+        when internal_spi in ( select spi from pc_vicmap_parcel where spi <> '' ) then '=HYPERLINK("https://vicmap.pozi.com/?parcelspi=' || internal_spi || '" , "' || internal_spi || ' map")'
+        else ''
+    end as internal_spi_map,
     case
         when internal_spi in ( select spi from pc_vicmap_parcel where spi <> '' ) then 'Y'
         else 'N'
     end as internal_spi_in_vicmap,
+    constructed_spi,
     case
-        when constructed_spi in ( select spi from pc_vicmap_parcel where spi <> '' ) then '=HYPERLINK("https://vicmap.pozi.com/?parcelspi=' || constructed_spi || '" , "' || constructed_spi || '")'
-        else constructed_spi
-    end as constructed_spi,
+        when constructed_spi in ( select spi from pc_vicmap_parcel where spi <> '' ) then '=HYPERLINK("https://vicmap.pozi.com/?parcelspi=' || constructed_spi || '" , "' || constructed_spi || ' map")'
+        else ''
+    end as constructed_spi_map,
     case
         when constructed_spi in ( select spi from pc_vicmap_parcel where spi <> '' ) then 'Y'
         else 'N'
     end as constructed_spi_in_vicmap,
-    ifnull ( ( select '=HYPERLINK("https://vicmap.pozi.com/?parcelspi=' || spi || '" , "' || spi || '")' from pc_vicmap_parcel vp where vp.crefno <> '' and vp.crefno = x.crefno and vp.spi <> internal_spi and vp.spi <> constructed_spi ) , '' ) as alt_spi
+    ifnull ( ( select spi from pc_vicmap_parcel vp where vp.crefno <> '' and vp.crefno = x.crefno and vp.spi <> internal_spi and vp.spi <> constructed_spi ) , '' ) as alt_spi,
+    ifnull ( ( select '=HYPERLINK("https://vicmap.pozi.com/?parcelspi=' || spi || '" , "' || spi || '")' from pc_vicmap_parcel vp where vp.crefno <> '' and vp.crefno = x.crefno and vp.spi <> internal_spi and vp.spi <> constructed_spi ) , '' ) as alt_spi_map
 from pc_council_parcel x
