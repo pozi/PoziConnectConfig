b35716f386f3679fe3ab5ea266e5a265dc74b7f3
Simon O'Keefe <simon.okeefe@groundtruth.com.au>
2020-03-12 10:20:12 +1100

    Manningham: cater for '0' values in floor number field

--- Manningham/SQL/Manningham PC Council Property Address.sql
+++ Manningham/SQL/Manningham PC Council Property Address.sql
@@ -85,7 +85,7 @@ select
         when upper ( A.floor_desc ) in ( 'L' , 'LVL' , 'LEVEL' , 'LEV' ) then 'L'
         when upper ( A.floor_desc ) in ( 'G' , 'GRD' ) then 'G'
         when upper ( A.floor_desc ) in ( 'LL' , 'LOWER' ) then 'LL'
-        when upper ( ifnull ( A.floor_desc , '' ) ) = '' and ifnull ( A.floor_no , '' ) <> '' then 'FL'
+        when upper ( ifnull ( A.floor_desc , '' ) ) = '' and ifnull ( A.floor_no , '' ) not in ( '' , 0 ) then 'FL'
         when A.property_name like '%ground floor%' then 'G'
         when A.property_name like '%floor%' then 'FL'
         when A.property_name like '%level%' then 'L'
