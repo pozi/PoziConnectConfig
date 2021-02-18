1d51f767b8944e4b03af25f19e5835c43e6e4f25
Simon O'Keefe <simon.okeefe@pozi.com>
2021-02-18 15:30:44 +1100

    Surf Coast: update config for Authority 7.1

--- Surf Coast/SQL/Surf Coast PC Council Property Address.sql
+++ Surf Coast/SQL/Surf Coast PC Council Property Address.sql
@@ -72,14 +72,7 @@ select distinct
         when auprstad.str_typ in ( 'STN' , 'STS' , 'STE' , 'STW' , 'STX' ) THEN 'STREET'
         else upper ( aualrefs.dsc_no3 )
     end as road_type,
-    case
-        when upper ( auprstad.str_typ ) in ( 'AVEN' , 'RDN' , 'STN' ) then 'N'
-        when upper ( auprstad.str_typ ) in ( 'AVES' , 'RDS' , 'STS' ) then 'S'
-        when upper ( auprstad.str_typ ) in ( 'AVEE' , 'RDE' , 'STE' ) then 'E'
-        when upper ( auprstad.str_typ ) in ( 'AVEW' , 'RDW' , 'STW' ) then 'W'
-        when upper ( auprstad.str_typ ) in ( 'AVEX' , 'RDX' , 'STX' ) then 'EX'
-        else ''
-    end as road_suffix,
+    ifnull ( ausrmast.str_suf , '' ) as road_suffix,
     upper ( auprstad.sbr_nme ) as locality_name,
     '' as postcode,
     '' as access_type,
@@ -94,7 +87,8 @@ from
     authority_auprparc auprparc join
     authority_aurtmast aurtmast on auprparc.ass_num = aurtmast.ass_num join
     authority_auprstad auprstad on auprparc.pcl_num = auprstad.pcl_num left join
-    authority_aualrefs aualrefs on auprstad.str_typ = aualrefs.ref_val and aualrefs.ref_typ = 'str_typ'
+    authority_aualrefs aualrefs on auprstad.str_typ = aualrefs.ref_val and aualrefs.ref_typ = 'str_typ' left join
+    authority_ausrmast ausrmast on auprstad.str_num = ausrmast.str_num
 where
     auprparc.pcl_flg in ( 'R' , 'P' , 'C' )
 )
