29c7f466d0a2165f9e02d25e2717d82c8bad830d
Simon O'Keefe <simon.okeefe@pozi.com>
2021-12-30 14:58:36 +1100

    Central Goldfields: use combined house number in dist-rel join

--- Central Goldfields/SQL/Central Goldfields PC Council Rural Address.sql
+++ Central Goldfields/SQL/Central Goldfields PC Council Rural Address.sql
@@ -1,10 +1,10 @@
 update pc_council_property_address
 set
-    distance_related_flag = ifnull ( ( select substr ( Distance_Related_Flag__Yes_No_ , 1 , 1 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.house_number_1 || pc_council_property_address.house_suffix_1 ) , '' ) ,
-    easting =  ifnull ( ( select round ( X ( geometry ) , 0 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.house_number_1 || pc_council_property_address.house_suffix_1 ) , '' ) ,
-    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.house_number_1 || pc_council_property_address.house_suffix_1 ) , '' ) ,
-    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.house_number_1 || pc_council_property_address.house_suffix_1 ) , '' ) ,
-    outside_property = ifnull ( ( select substr ( Outside_Property__Yes_No_ , 1 , 1 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.house_number_1 || pc_council_property_address.house_suffix_1 ) , '' )
+    distance_related_flag = ifnull ( ( select substr ( Distance_Related_Flag__Yes_No_ , 1 , 1 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.num_address ) , '' ) ,
+    easting =  ifnull ( ( select round ( X ( geometry ) , 0 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.num_address ) , '' ) ,
+    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.num_address ) , '' ) ,
+    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.num_address ) , '' ) ,
+    outside_property = ifnull ( ( select substr ( Outside_Property__Yes_No_ , 1 , 1 ) from cgsc_rural_address ra where ra.Property_Number = pc_council_property_address.propnum and ra.House_Number_1 = pc_council_property_address.num_address ) , '' )
 where
     propnum in ( select Property_Number from cgsc_rural_address )
     
