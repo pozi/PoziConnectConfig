3464c98e166cca57e88cd18183287ca666530ffd
Terry McDonell <35645950+terry-longmacch@users.noreply.github.com>
2022-08-17 16:36:34 +1000

    Towong: switch data source to CouncilWise & move DB File

--- Towong/SQL/Towong PC Council Parcel.sql
+++ Towong/SQL/Towong PC Council Parcel.sql
@@ -6,14 +6,13 @@ from
 select
     *,
     case
-        when internal_spi <> '' then 'council_spi'
-        when constructed_spi <> '' then 'council_attributes'
-        else ''
-    end as source,
-    case
         when internal_spi <> '' then internal_spi
         else constructed_spi
-    end as spi
+    end as spi,
+    case
+        when internal_spi <> '' then 'council_spi'
+        else 'council_attributes'
+    end as spi_source
 from
 (
 select
@@ -38,145 +37,128 @@ select
 from
 (
 select
-    case
-        when Parcel.Lot like 'CM%' then 'NCPR'
-        else cast ( Parcel.PropertyNumber as varchar )
-    end as propnum,
+    cast ( propertyNumber as varchar ) as propnum,
     '' as status,
-    cast ( Parcel.LandParcelNumber as varchar ) as crefno,
-    ifnull ( Parcel.StandardParcelId , '' ) as internal_spi,
-    '' as summary,
-    '' as part,
-    case Parcel.Type
-        when 'Lodged Plan' then 'LP' || Parcel.PlanNo
-        when 'Title Plan' then 'TP' || Parcel.PlanNo
-        when 'Plan of Subdivision' then 'PS' || Parcel.PlanNo
-        when 'Consolidation Plan' then 'PC' || Parcel.PlanNo
-        when 'Strata Plan' then 'RP' || Parcel.PlanNo
-        when 'Stratum Plan' then 'SP' || Parcel.PlanNo
+    cast ( parcelId as varchar ) as crefno,
+    case
+        when planPrefix = 'CA' then ''
+        else replace ( ifnull ( vicParcelSpi , '' ), '/' , '\' )
+    end as internal_spi,
+    case
+        when isPartOfLot = 'Yes' then 'P'
         else ''
+    end as part,
+    case
+        when planPrefix = 'CA' then ''
+        else planPrefix || cast ( planNo as varchar )
     end as plan_number,
-    case Parcel.Type
-        when 'Lodged Plan' then 'LP'
-        when 'Title Plan' then 'TP'
-        when 'Plan of Subdivision' then 'PS'
-        when 'Consolidation Plan' then 'PC'
-        when 'Strata Plan' then 'RP'
-        when 'Stratum Plan' then 'SP'
-        else ''
+    case
+        when planPrefix = 'CA' then ''
+        else planPrefix
     end as plan_prefix,
-    Parcel.PlanNo as plan_numeral,
+    ifnull ( planNo , '' ) as plan_numeral,
+    ifnull ( lot , '' ) as lot_number,
+    ifnull ( crownAllotment , '' ) as allotment,
     case
-        when Parcel.Lot = 'CROWN LAND' then ''
-        when Parcel.Lot glob '?(*' then substr ( Parcel.Lot , 1 , 1 )
-        when Parcel.Lot glob '??(*' then substr ( Parcel.Lot , 1 , 2 )
-        else upper ( Parcel.Lot )
-    end as lot_number,
-    Parcel.CrownAllotment as allotment,
-    case
-        when upper ( Parcel.Section ) in ( 'NO' , 'NO SEC' ) then ''
-        else Parcel.Section
-    end as sec,
-    case
-        when Parcel.Lot like '%(BLK%)' then substr ( Parcel.Lot , length ( Parcel.Lot ) - 1 , 1 )
+        when planPrefix = 'CA' then ifnull ( section , '' )
         else ''
-    end as block,
-    '' as portion,
-    '' as subdivision,
-    case upper ( Parcel.Parish )
-        when 'ADJIE' then '2003'
-        when 'BEETHANG' then '2103'
-        when 'BENAMBRA' then '2114'
-        when 'BERRINGA' then '2126'
-        when 'BERRINGAMA' then '2127'
-        when 'BOGONG NORTH' then '2164'
-        when 'BOGONG SOUTH' then '2165'
-        when 'BOLGA' then '2173'
-        when 'BONEGILLA' then '2178'
-        when 'BOORGUNYAH' then '2191'
-        when 'BULLIOH' then '2266'
-        when 'BUNGIL' then '2283'
-        when 'BUNGIL EAST' then '2284'
-        when 'BURROWYE' then '2303'
-        when 'BURRUNGABUGGE' then '2307'
-        when 'CANABORE' then '2327'
-        when 'CANBORE' then '2327'
-        when 'COLAC COLAC' then '2406'
-        when 'CORRYONG' then '2457'
-        when 'CUDGEWA' then '2471'
-        when 'DARTELLA' then '2500'
-        when 'DORCHAP' then '2539'
-        when 'GIBBO' then '2652'
-        when 'GRANYA' then '2720'
-        when 'GUNDOWRING' then '2734'
-        when 'GUNGARLAN' then '2735'
-        when 'INDI' then '2772'
-        when 'JEMBA' then '2791'
-        when 'JINGELLIC' then '2803'
-        when 'JINJELLIC' then '2803'
-        when 'KANCOBIN' then '2828'
-        when 'KEELANGIE' then '2855'
-        when 'KOETONG' then '2895'
-        when 'KOSCIUSKO' then '2931'
-        when 'MAGORRA' then '3033'
-        when 'MALKARA' then '3043'
-        when 'MITTA MITTA' then '3127'
-        when 'MOWAMBA' then '3203'
-        when 'MOYANGUL' then '3204'
-        when 'MULLAGONG' then '3213'
-        when 'NARIEL' then '3266'
-        when 'NOORONGONG' then '3322'
-        when 'PINNIBAR' then '3399'
-        when 'TALGARNO' then '3524'
-        when 'TALLANDOON' then '3527'
-        when 'TANGAMBALANGA' then '3541'
-        when 'TATONGA' then '3566'
-        when 'THOLOGOLONG' then '3584'
-        when 'THOWGLA' then '3588'
-        when 'THOWLGA' then '3588'
-        when 'TINTALDRA' then '3599'
-        when 'TONGARO' then '3603'
-        when 'TOWONG' then '3644'
-        when 'UNDOWAH' then '3683'
-        when 'WABBA' then '3691'
-        when 'WAGRA' then '3698'
-        when 'WALLABY' then '3704'
-        when 'WALWA' then '3717'
-        when 'WELUMLA' then '3791'
-        when 'WERMATONG' then '3795'
-        when 'WOLLONABY' then '3856'
-        when 'WYEEBOO' then '3918'
-        when 'YABBA' then '3927'
+    end as sec,
+    ifnull ( block , '' ) as block,
+    ifnull ( portion , '' ) as portion,
+    ifnull ( subdivision , '' ) as subdivision,
+    case
+        when planPrefix <> 'CA' then ''
+        when upper ( parish ) = 'ADJIE' then '2003'
+        when upper ( parish ) = 'BEETHANG' then '2103'
+        when upper ( parish ) = 'BENAMBRA' then '2114'
+        when upper ( parish ) = 'BERRINGA' then '2126'
+        when upper ( parish ) = 'BERRINGAMA' then '2127'
+        when upper ( parish ) = 'BOGONG NORTH' then '2164'
+        when upper ( parish ) = 'BOGONG SOUTH' then '2165'
+        when upper ( parish ) = 'BOLGA' then '2173'
+        when upper ( parish ) = 'BONEGILLA' then '2178'
+        when upper ( parish ) = 'BOORGUNYAH' then '2191'
+        when upper ( parish ) = 'BULLIOH' then '2266'
+        when upper ( parish ) = 'BUNGIL' then '2283'
+        when upper ( parish ) = 'BUNGIL EAST' then '2284'
+        when upper ( parish ) = 'BURROWYE' then '2303'
+        when upper ( parish ) = 'BURRUNGABUGGE' then '2307'
+        when upper ( parish ) = 'CANABORE' then '2327'
+        when upper ( parish ) = 'CANBORE' then '2327'
+        when upper ( parish ) = 'COLAC COLAC' then '2406'
+        when upper ( parish ) = 'CORRYONG' then '2457'
+        when upper ( parish ) = 'CUDGEWA' then '2471'
+        when upper ( parish ) = 'DARTELLA' then '2500'
+        when upper ( parish ) = 'DORCHAP' then '2539'
+        when upper ( parish ) = 'GIBBO' then '2652'
+        when upper ( parish ) = 'GRANYA' then '2720'
+        when upper ( parish ) = 'GUNDOWRING' then '2734'
+        when upper ( parish ) = 'GUNGARLAN' then '2735'
+        when upper ( parish ) = 'INDI' then '2772'
+        when upper ( parish ) = 'JEMBA' then '2791'
+        when upper ( parish ) = 'JINGELLIC' then '2803'
+        when upper ( parish ) = 'JINJELLIC' then '2803'
+        when upper ( parish ) = 'KANCOBIN' then '2828'
+        when upper ( parish ) = 'KEELANGIE' then '2855'
+        when upper ( parish ) = 'KOETONG' then '2895'
+        when upper ( parish ) = 'KOSCIUSKO' then '2931'
+        when upper ( parish ) = 'MAGORRA' then '3033'
+        when upper ( parish ) = 'MALKARA' then '3043'
+        when upper ( parish ) = 'MITTA MITTA' then '3127'
+        when upper ( parish ) = 'MOWAMBA' then '3203'
+        when upper ( parish ) = 'MOYANGUL' then '3204'
+        when upper ( parish ) = 'MULLAGONG' then '3213'
+        when upper ( parish ) = 'NARIEL' then '3266'
+        when upper ( parish ) = 'NOORONGONG' then '3322'
+        when upper ( parish ) = 'PINNIBAR' then '3399'
+        when upper ( parish ) = 'TALGARNO' then '3524'
+        when upper ( parish ) = 'TALLANDOON' then '3527'
+        when upper ( parish ) = 'TANGAMBALANGA' then '3541'
+        when upper ( parish ) = 'TATONGA' then '3566'
+        when upper ( parish ) = 'THOLOGOLONG' then '3584'
+        when upper ( parish ) = 'THOWGLA' then '3588'
+        when upper ( parish ) = 'THOWLGA' then '3588'
+        when upper ( parish ) = 'TINTALDRA' then '3599'
+        when upper ( parish ) = 'TONGARO' then '3603'
+        when upper ( parish ) = 'TOWONG' then '3644'
+        when upper ( parish ) = 'UNDOWAH' then '3683'
+        when upper ( parish ) = 'WABBA' then '3691'
+        when upper ( parish ) = 'WAGRA' then '3698'
+        when upper ( parish ) = 'WALLABY' then '3704'
+        when upper ( parish ) = 'WALWA' then '3717'
+        when upper ( parish ) = 'WELUMLA' then '3791'
+        when upper ( parish ) = 'WERMATONG' then '3795'
+        when upper ( parish ) = 'WOLLONABY' then '3856'
+        when upper ( parish ) = 'WYEEBOO' then '3918'
+        when upper ( parish ) = 'YABBA' then '3927'
         else ''
     end as parish_code,
-    case upper ( Parcel.Township )
-        when 'TOWONG' then '5799'
-        when 'NARIEL' then '5575'
-        when 'CORRYONG' then '5200'
-        when 'DARTMOUTH' then '5230'
-        when 'BEETOOMBA' then '5063'
-        when 'CRAVENSVILLE' then '5208'
-        when 'BERRINGAMA' then '5074'
-        when 'GRANITE FLAT' then '5345'
-        when 'MITTA MITTA' then '5539'
-        when 'KOETONG' then '5425'
-        when 'TALLANGATTA VALLEY' then '5758'
-        when 'ESKDALE' then '5285'
-        when 'GRANYA' then '5349'
-        when 'TALLANGATTA' then '5757'
-        when 'TATONGA' then '5772'
-        when 'BETHANGA' then '5079'
+    case
+        when upper ( township ) = 'TOWONG' then '5799'
+        when upper ( township ) = 'NARIEL' then '5575'
+        when upper ( township ) = 'CORRYONG' then '5200'
+        when upper ( township ) = 'DARTMOUTH' then '5230'
+        when upper ( township ) = 'BEETOOMBA' then '5063'
+        when upper ( township ) = 'CRAVENSVILLE' then '5208'
+        when upper ( township ) = 'BERRINGAMA' then '5074'
+        when upper ( township ) = 'GRANITE FLAT' then '5345'
+        when upper ( township ) = 'MITTA MITTA' then '5539'
+        when upper ( township ) = 'KOETONG' then '5425'
+        when upper ( township ) = 'TALLANGATTA VALLEY' then '5758'
+        when upper ( township ) = 'ESKDALE' then '5285'
+        when upper ( township ) = 'GRANYA' then '5349'
+        when upper ( township ) = 'TALLANGATTA' then '5757'
+        when upper ( township ) = 'TATONGA' then '5772'
+        when upper ( township ) = 'BETHANGA' then '5079'
         else ''
     end as township_code,
+    ifnull ( legacyData1 , '' ) as summary,
     '367' as lga_code,
-    cast ( Parcel.PropertyNumber as varchar ) as assnum
+    '' as assnum
 from
-    lynx_vwlandparcel Parcel join
-    lynx_propertys Property on Parcel.PropertyNumber = Property.Property
+    councilwise_parcels
 where
-    Parcel.Status = 'Active' and
-    Parcel.Ended is null and
-    Property.Type not in ( 672 , 700 )
+    parcelStatus = 1
 )
 )
 )
--- Towong/SQL/Towong PC Council Property Address.sql
+++ Towong/SQL/Towong PC Council Property Address.sql
@@ -27,27 +27,20 @@ select
         rtrim ( ' ' || road_suffix ) ) as road_name_combined
 from (
 
-select
-    cast ( Property.Property as varchar ) as propnum,
+select distinct
+    cast ( propertyNumber as varchar ) as propnum,
     '' as status,
     '' as base_propnum,
     '' as is_primary,
     '' as distance_related_flag,
     '' as hsa_flag,
     '' as hsa_unit_id,
-    '' as location_descriptor,
     '' as blg_unit_type,
     '' as blg_unit_prefix_1,
-    case
-        when substr ( Property.UnitNo , 2 , 1 ) = '-' then ifnull ( substr ( Property.UnitNo , 1 , 1 ) , '' )
-        else ifnull ( Property.UnitNo , '' )
-    end as blg_unit_id_1,
+    ifnull ( cast ( unitNumber as varchar ) , '' ) as blg_unit_id_1,
     '' as blg_unit_suffix_1,
     '' as blg_unit_prefix_2,
-    case
-        when substr ( Property.UnitNo , 2 , 1 ) = '-' then ifnull ( substr ( Property.UnitNo , 3 , 99 ) , '' )
-        else ''
-    end as blg_unit_id_2,
+    '' as blg_unit_id_2,
     '' as blg_unit_suffix_2,
     '' as floor_type,
     '' as floor_prefix_1,
@@ -56,45 +49,30 @@ select
     '' as floor_prefix_2,
     '' as floor_no_2,
     '' as floor_suffix_2,
-    ifnull ( Property.Name , '' ) as building_name,
+    '' as building_name,
     '' as complex_name,
+    '' as location_descriptor,
     '' as house_prefix_1,
-    case
-        when substr ( Property.StreetNofrom , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-        when substr ( Property.StreetNofrom , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then Property.StreetNofrom
-        else ifnull ( substr ( Property.StreetNofrom , 1 , length ( Property.StreetNofrom ) - 1 ) , '' ) 
-    end as house_number_1,
-    case
-        when substr ( Property.StreetNofrom , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-        when substr ( Property.StreetNofrom , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-        else ifnull ( upper ( substr ( Property.StreetNofrom , -1 , 1 ) ) , '' )
-    end as house_suffix_1,
+    ifnull ( cast ( fromStreetNumber as varchar ) , '' ) as house_number_1,
+    ifnull ( cast ( fromStreetNumberSuffix as varchar ) , '' ) as house_suffix_1,
     '' as house_prefix_2,
+    ifnull ( cast ( toStreetNumber as varchar ) , '' ) as house_number_2,
+    ifnull ( cast ( toStreetNumberSuffix as varchar ) , '' ) as house_suffix_2,
+    replace ( upper ( streetNameOnly ) , ' - ' , '-' ) as road_name,
     case
-        when substr ( Property.StreetNoTo , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-        when substr ( Property.StreetNoTo , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then Property.StreetNoTo 
-        else ifnull ( substr ( Property.StreetNoTo , 1 , length ( Property.StreetNoTo ) - 1 ) , '' ) 
-    end as house_number_2,
-    case
-        when substr ( Property.StreetNoTo , 1 , 1 ) not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-        when substr ( Property.StreetNoTo , -1 , 1 ) in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') then ''
-        else ifnull ( upper ( substr ( Property.StreetNoTo , -1 , 1 ) ) , '' )
-    end as house_suffix_2,
-    replace ( upper ( Street.Name ) , ' - ' , '-' ) as road_name,
-    case
-        when StreetType.Type like 'Road %' then 'ROAD'
-        when StreetType.Type like 'Street %' then 'STREET'
-        else upper ( StreetType.Type )
+        when streetType like 'Road %' then 'ROAD'
+        when streetType like 'Street %' then 'STREET'
+        else upper ( streetType )
     end as road_type, 
     case
-        when StreetType.Type like '% North' then 'N'
-        when StreetType.Type like '% South' then 'S'
-        when StreetType.Type like '% East' then 'E'
-        when StreetType.Type like '% West' then 'W'
+        when streetType like '% North' then 'N'
+        when streetType like '% South' then 'S'
+        when streetType like '% East' then 'E'
+        when streetType like '% West' then 'W'
         else ''
     end as road_suffix,
-    upper ( Locality.Locality ) as locality_name,
-    Locality.Postcode as postcode,
+    upper ( suburb ) as locality_name,
+    '' as postcode,
     '' as access_type,
     '' as easting,
     '' as northing,
@@ -102,20 +80,10 @@ select
     '' as outside_property,
     '367' as lga_code,
     '' as crefno,
-    '' as summary
+    ifnull ( formattedAddress , '' ) as summary
 from
-    lynx_vwpropertyclassification Classification,
-    lynx_propertys Property,
-    lynx_streets Street,
-    lynx_streettype StreetType,
-    lynx_localities Locality
-where
-    Classification.PropertyNumber = Property.Property and
-    Property.StreetID = Street.ID and
-    Street.Type = StreetType.ID and
-    Street.Locality = Locality.ID and
-    Property.Type not in ( 672 , 700 ) and
-    Classification.LandClassificationCode <> 010
+    councilwise_properties
+
 )
 )
 )
