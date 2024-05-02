d096c715ce856d3a99413ec6d3f18b1c303fa969
Terry McDonell <35645950+terry-longmacch@users.noreply.github.com>
2024-05-02 14:17:36 +1000

    Southern Grampians:  Fix issues from initial install

--- Southern Grampians/SQL/Southern Grampians PC Council Rural Address.sql
+++ Southern Grampians/SQL/Southern Grampians PC Council Rural Address.sql
@@ -1,10 +1,10 @@
 update pc_council_property_address
 set
-    distance_related_flag = ifnull ( ( select 'Y' from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    easting = ifnull ( ( select cast ( cast ( ra.easting as integer ) as varchar ) from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    northing = ifnull ( ( select cast ( cast ( ra.northing as integer ) as varchar ) from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    datum_proj = ifnull ( ( select ra.datum_proj from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    outside_property = ifnull ( ( select ra.outside_property from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
-    is_primary = ifnull ( ( select ra.is_primary from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' )
+    is_primary = ifnull ( ( select is_primary from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    distance_related_flag =  ifnull ( ( select dist_related_flag from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    easting =  ifnull ( ( select round ( X ( geometry ) , 0 ) from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    northing = ifnull ( ( select round ( Y ( geometry ) , 0 ) from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    datum_proj = ifnull ( ( select 'EPSG:' || SRID ( geometry ) from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) ,
+    outside_property =  ifnull ( ( select outside_property from sgsc_rural_address ra where ra.propnum = pc_council_property_address.propnum and ra.house_number_1 = pc_council_property_address.house_number_1 ) , '' ) 
 where
-    propnum in ( select propnum from sgsc_rural_address ra where ra.propnum <> '' )
+    propnum in ( select propnum from sgsc_rural_address where geometry is not null )
